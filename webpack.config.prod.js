d0063d260c2913983a57bd23219ff51219b79400
and80506 <and80506@163.com>
2018-03-21 14:18:02 +0800

    打包调试；

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -68,7 +68,7 @@ export default {
   target: 'web', // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
   output: {
     path: `${__dirname}/dist/static`,
-    publicPath: '/public/',
+    publicPath: '/static/',
     filename: '[name].[chunkhash].js',
     chunkFilename: '[name].[chunkhash].js'
   },
