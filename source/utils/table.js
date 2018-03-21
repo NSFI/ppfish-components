95c200b92df63fb1e38dc807b68f67d2f4f1f5ed
wangxiaoling <wangxiaoling@corp.netease.com>
2018-03-21 20:06:40 +0800

    table报错问题修复

--- source/utils/table.js
+++ source/utils/table.js
@@ -1,3 +1,8 @@
+/**
+ * 获取同级的相邻节点
+ * @param {object} elem - 节点
+ * @returns {Array} 相邻节点数组
+ */
 export const getSiblings = (elem) => {
   let siblings = [];
   let sibling = elem.parentNode.firstChild;
@@ -7,7 +12,14 @@ export const getSiblings = (elem) => {
   return siblings;
 };
 
+/**
+ * 设置Table列表激活样式
+ * @description 策划要求：被点击的列表加载active样式
+ * @param {object} node - 所需要添加样式的节点或子节点
+ */
 export const setRowActive = (node) => {
+  //去除个例外
+  if (node.classList.contains('ant-tooltip-inner') || node.classList.contains('ant-tooltip-arrow')) return;
   while (!node.classList.contains('ant-table-row')) {
     node = node.parentNode;
     if (node.classList.contains('ant-table-row')) {
