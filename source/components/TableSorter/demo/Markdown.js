b220df437a9994c032a43ef49199b8e2011a4402
qingze <hzqingze@corp.netease.com>
2018-05-15 14:41:05 +0800

    TableSorter

---
+++ source/components/TableSorter/demo/Markdown.js
@@ -0,0 +1,10 @@
+let markDownAPI=`
+TableSorter文档
+Name | Academy | score 
+:-: | :-: | :-: 
+Harry Potter | Gryffindor| 90 
+Hermione Granger | Gryffindor | 100 
+Draco Malfoy | Slytherin | 90
+`;
+
+export default markDownAPI;
