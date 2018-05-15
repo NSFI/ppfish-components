df139989f17bff0e1d94aa39d45f2693fa51d757
qingze <hzqingze@corp.netease.com>
2018-05-15 14:41:05 +0800

    TableSorter

--- source/routers/index.js
+++ source/routers/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route, IndexRoute, browserHistory, IndexRedirect } from 'react-router';
+import {Router, Route, IndexRoute, browserHistory, IndexRedirect} from 'react-router';
 import App from '../components/App';
 import AnimationImageLoader from '../components/AnimationImageLoader/demo/App';
 import Avatar from '../components/Avatar/demo/App';
@@ -14,7 +14,7 @@ import NumberCounter from '../components/NumberCounter/demo/App';
 import SearchInput from '../components/SearchInput/demo/App';
 import StickVerticalMenu from '../components/StickVerticalMenu/demo/App';
 import Suggest from '../components/Suggest/demo/App';
-// import TableSorter from '../components/TableSorter/demo/App';
+import TableSorter from '../components/TableSorter/demo/App';
 import TextOverflow from '../components/TextOverflow/demo/App';
 import TimePicker from '../components/TimePicker/demo/App';
 import TreeSelect from '../components/TreeSelect/demo/App';
@@ -25,24 +25,26 @@ class RouteMap extends React.Component {
     return (
       <Router history={browserHistory}>
         <Route path="/" component={App}>
-          <IndexRoute component={AnimationImageLoader} />
-          <Route path="/**/AnimationImageLoader/" component={AnimationImageLoader} />
-          <Route path="/**/Avatar/" component={Avatar} />
-          <Route path="/**/Collapse/" component={Collapse} />
-          <Route path="/**/ImageLoader/" component={ImageLoader} />
-          <Route path="/**/Drawer/" component={Drawer} />
-          <Route path="/**/Loading/" component={Loading} />
-          <Route path="/**/NumberCounter/" component={NumberCounter} />
-          <Route path="/**/SearchInput/" component={SearchInput} />
-          <Route path="/**/StickVerticalMenu/" component={StickVerticalMenu} />
-          <Route path="/**/Suggest/" component={Suggest} />
-          <Route path="/**/TextOverflow/" component={TextOverflow} />
-          <Route path="/**/TimePicker/" component={TimePicker} />
-          <Route path="/**/TreeSelect/" component={TreeSelect} />
-          <Route path="/**/UI/" component={UIKJGF} />
+          <IndexRoute component={AnimationImageLoader}/>
+          <Route path="/**/AnimationImageLoader/" component={AnimationImageLoader}/>
+          <Route path="/**/Avatar/" component={Avatar}/>
+          <Route path="/**/Collapse/" component={Collapse}/>
+          <Route path="/**/ImageLoader/" component={ImageLoader}/>
+          <Route path="/**/Drawer/" component={Drawer}/>
+          <Route path="/**/Loading/" component={Loading}/>
+          <Route path="/**/NumberCounter/" component={NumberCounter}/>
+          <Route path="/**/SearchInput/" component={SearchInput}/>
+          <Route path="/**/StickVerticalMenu/" component={StickVerticalMenu}/>
+          <Route path="/**/Suggest/" component={Suggest}/>
+          <Route path="/**/TextOverflow/" component={TextOverflow}/>
+          <Route path="/**/TimePicker/" component={TimePicker}/>
+          <Route path="/**/TreeSelect/" component={TreeSelect}/>
+          <Route path="/**/TableSorter/" component={TableSorter}/>
+          <Route path="/**/UI/" component={UIKJGF}/>
         </Route>
       </Router>
     );
   }
 }
+
 export default RouteMap;
