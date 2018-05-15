39b7dea8febdc3b80743a8d6ec0ba1abf89c1836
qingze <hzqingze@corp.netease.com>
2018-05-15 15:00:56 +0800

    add CustomTable

---
+++ source/components/CustomTable/demo/Markdown.js
@@ -0,0 +1,10 @@
+let markDownAPI=`
+CustomTable文档
+Name | Academy | score 
+:-: | :-: | :-: 
+Harry Potter | Gryffindor| 90 
+Hermione Granger | Gryffindor | 100 
+Draco Malfoy | Slytherin | 90
+`;
+
+export default markDownAPI;
