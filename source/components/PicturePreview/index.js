05405ff926face8f4340bc5d915273cc7dbdc7cf
zhiyou <gtopia@163.com>
2018-06-07 19:15:00 +0800

    重构PicturePreview

--- source/components/PicturePreview/index.js
+++ source/components/PicturePreview/index.js
@@ -129,25 +129,23 @@ class PicturePreview extends Component {
     return (
       <Modal
         title=""
-        width={978}
-        wrapClassName="vertical-center-modal"
+        width={"100%"}
+        wrapClassName="modal-wrap"
         visible={visible}
         footer={null}
+        mask={true}
         destroyOnClose={true}
         closable={false}
         onCancel={this.handleOnClose}
       >
         <div className={carouselWrapClass} ref={node => this.carouselWrap = node}>
-          <div className="left-side side" style={{display: source.length > 1 ? "flex" : "none"}}>
-            <i className="iconfont icon-zuojiantou1" onClick={() => this.carousel.prev()}/>
-          </div>
           <div className="middle">
             <Carousel
               focusOnSelect={true}
               dots={dots}
               effect={"fade"}
               initialSlide={activeIndex}
-              ref={carousel => this.carousel = carousel}
+              ref={node => this.carousel = node}
             >
               {
                 source.map((each, index) => {
@@ -157,15 +155,6 @@ class PicturePreview extends Component {
                     <div key={index} className="picture-container">
                       <div className="img-wrap">
                         <img src={each.url} width={getWidthAndHeight(imgWidth, imgHeight).width} height={getWidthAndHeight(imgWidth, imgHeight).height}/>
-                        <i className="iconfont icon-guanbi" onClick={this.handleOnClose}/>
-                      </div>
-                      <div className={operateClass}>
-                        <i className="iconfont icon-dengbi" onClick={this.handleDengbi}/>
-                        <i className={fullscreenClass} onClick={this.handleFullscreen}/>
-                        <i className="iconfont icon-fangda" onClick={this.handleZoomIn}/>
-                        <i className="iconfont icon-suoxiao" onClick={this.handleZoomOut}/>
-                        <i className="iconfont icon-xuanzhuan" onClick={this.handleRotate}/>
-                        <a download href={each.url} className="iconfont icon-save"></a>
                       </div>
                     </div>
                   );
@@ -173,8 +162,24 @@ class PicturePreview extends Component {
               }
             </Carousel>
           </div>
+
+          <i className="iconfont icon-guanbi" onClick={this.handleOnClose}/>
+
+          <div className="left-side side" style={{display: source.length > 1 ? "flex" : "none"}}>
+            <i className="iconfont icon-zuojiantou1" onClick={() => this.carousel.prev()}/>
+          </div>
+
           <div className="right-side side" style={{display: source.length > 1 ? "flex" : "none"}}>
             <i className="iconfont icon-youjiantou1" onClick={() => this.carousel.next()}/>
+          </div>
+
+          <div className={operateClass}>
+            <i className="iconfont icon-dengbi" onClick={this.handleDengbi}/>
+            <i className={fullscreenClass} onClick={this.handleFullscreen}/>
+            <i className="iconfont icon-fangda" onClick={this.handleZoomIn}/>
+            <i className="iconfont icon-suoxiao" onClick={this.handleZoomOut}/>
+            <i className="iconfont icon-xuanzhuan" onClick={this.handleRotate}/>
+            <a download href={'each.url'} className="iconfont icon-save"></a>
           </div>
         </div>
       </Modal>
