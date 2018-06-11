b78fd83e9c7dbfbdb0b743519ff0eadc5c3b5250
zhiyou <gtopia@163.com>
2018-06-11 20:15:21 +0800

    处理图片切换前后的状态

--- source/components/PicturePreview/index.js
+++ source/components/PicturePreview/index.js
@@ -7,19 +7,22 @@ import { fullscreen, exitfullscreen, addFullscreenchangeEvent, checkFullscreen }
 import './index.less';
 
 /*
-  图片可视区默认宽高
+  常量定义
  */
-const maxVisualWidth = parseInt(window.innerWidth * 0.8, 10);
-const maxVisualHeight = parseInt(window.innerHeight * 0.8, 10);
+const maxVisualWidth = parseInt(window.innerWidth * 0.8, 10),   // 图片可视区默认宽度
+      maxVisualHeight = parseInt(window.innerHeight * 0.8, 10), // 图片可视区默认高度
+      ZOOM_FACTOR = 0.1,//图片缩放系数
+      MAX_SCALE = 5,    //最大的图片显示比例
+      MIN_SCALE = 0.1;  //最小的图片显示比例
 
 /**
  * 获取图片的自适应宽高
  * @param  {[Number]} w [图片原始宽度]
  * @param  {[Number]} h [图片原始高度]
  * @param  {[Boolean]} isFullscreen [是否为全屏状态]
- * @return {[Object]}   [包含自适应后宽高属性的对象]
+ * @return {[Object]}   [返回适应后的宽、高]
  */
-const getAdaptiveWH = (w, h, isFullscreen) => {
+const getAdaptiveImg = (w, h, isFullscreen) => {
   let obj = {
     width: w,
     height: h
@@ -55,7 +58,7 @@ const getAdaptiveWH = (w, h, isFullscreen) => {
 };
 
 function setStyle(el, css) {
-  for (var key in css) {
+  for (let key in css) {
     el.style[key] = css[key];
   }
 }
@@ -82,17 +85,32 @@ class PicturePreview extends Component {
 
   constructor(props) {
     super(props);
+
     this.hasAddExitfullscreenEvt = false;
+    this.prevIndex = 0;
+    this.selector = '.carousel-wrap .slick-list img';
+    this.imgs = this.props.source;
+
     this.state = {
       visible: this.props.visible,
-      isFullscreen: false
+      isFullscreen: false,
+      activeIndex: this.props.activeIndex,
+      isDisableDengbi: false,
+      isDisableFangda: false,
+      isDisableSuoxiao: false
     };
   }
 
   componentWillReceiveProps(nextProps) {
-    if ( this.state.visible != nextProps.visible ) {
+    if (this.state.visible != nextProps.visible) {
       this.setState({
         visible: nextProps.visible
+      });
+    }
+
+    if (this.state.activeIndex != nextProps.activeIndex) {
+      this.setState({
+        activeIndex: nextProps.activeIndex
       });
     }
   }
@@ -123,10 +141,6 @@ class PicturePreview extends Component {
     });
   };
 
-  handleDengbi = () => {
-
-  };
-
   handleFullscreen = () => {
     if (this.state.isFullscreen) {
       // 退出全屏
@@ -145,17 +159,42 @@ class PicturePreview extends Component {
     }
   };
 
-  handleZoom = (selector, isZoomIn) => {
-    let scale = 0.1,
-        img = document.querySelector(selector),
-        ratio = img.dataset.ratio,
-        zWidth = parseInt(img.width + img.height * scale * ratio, 10),
-        zHeight = parseInt(img.height + img.height * scale, 10);
+  handleZoom = (index, type) => {
+    let img = document.querySelectorAll(this.selector)[index],
+        imgInfo = this.imgs[index],
+        curScale = imgInfo.scale,
+        zWidth = imgInfo.naturalWidth,
+        zHeight = imgInfo.naturalHeight;
 
-    if (!isZoomIn) {
-      zWidth = parseInt(img.width - img.height * scale * ratio, 10);
-      zHeight = parseInt(img.height - img.height * scale, 10);
+    if (type == 'in') {
+      curScale += ZOOM_FACTOR;
+      if (curScale > MAX_SCALE) {
+        curScale = MAX_SCALE;
+        // TODO：禁用放大按钮点击
+      }
+      zWidth = parseInt(imgInfo.adaptiveWidth * curScale, 10);
+      zHeight = parseInt(imgInfo.adaptiveHeight * curScale, 10);
+    } else if (type == 'out') {
+      curScale -= ZOOM_FACTOR;
+      if (curScale < MIN_SCALE) {
+        curScale = MIN_SCALE;
+        // TODO：禁用缩小按钮点击
+      }
+      zWidth = parseInt(imgInfo.adaptiveWidth * curScale, 10);
+      zHeight = parseInt(imgInfo.adaptiveHeight * curScale, 10);
+    } else if (type == '1:1') {
+      curScale = 1;
+      zWidth = imgInfo.naturalWidth,
+      zHeight = imgInfo.naturalHeight;
+    } else if (type == 'reset') {
+      curScale = 1;
+      zWidth = imgInfo.adaptiveWidth;
+      zHeight = imgInfo.adaptiveHeight;
     }
+
+    imgInfo.scale = curScale;
+
+    // console.log('>> handleZoom: ', this.imgs);
 
     setStyle(img, {
       'width': zWidth + 'px',
@@ -163,13 +202,14 @@ class PicturePreview extends Component {
     });
   };
 
-  handleRotate = (selector) => {
-    let img = document.querySelector(selector),
-        oldVal = parseInt(img.dataset.rotate, 10) || 0,
-        newVal = oldVal + 90,
+  handleRotate = (index, isReset) => {
+    let img = document.querySelectorAll(this.selector)[index],
+        imgInfo = this.imgs[index],
+        oldVal = imgInfo.rotate || 0,
+        newVal = isReset ? 0 : (oldVal + 90),
         transform = 'rotate(' + newVal + 'deg)';
 
-    img.dataset.rotate = newVal;
+    imgInfo.rotate = newVal;
 
     setStyle(img, {
       '-webkit-transform': transform,
@@ -179,17 +219,67 @@ class PicturePreview extends Component {
   };
 
   handleSave = (selector, name) => {
-      let img = document.querySelector(selector),
-          a = document.createElement('a'),
-          event = new MouseEvent('click');
+    let img = document.querySelector(selector),
+        a = document.createElement('a'),
+        event = new MouseEvent('click');
 
-      a.download = name || '';
-      a.href = img.src;
-      a.dispatchEvent(event);
+    a.download = name || '';
+    a.href = img.src;
+    a.dispatchEvent(event);
+  };
+
+  /**
+   * 还原对上一张图片进行的操作
+   * @return {[type]} [description]
+   */
+  resetImg = () => {
+    let index = this.prevIndex;
+
+    this.handleZoom(index, 'reset');
+    this.handleRotate(index, true);
+  };
+
+  handleCarouselPrev = () => {
+    let curIndex = this.state.activeIndex,
+        totalNum = this.props.source.length;
+
+    this.prevIndex = curIndex;
+
+    if (curIndex == 0) {
+      curIndex = totalNum - 1;
+    } else {
+      curIndex -= 1;
+    }
+
+    this.setState({
+      activeIndex: curIndex,
+    }, () => {
+      this.carousel.prev();
+    });
+  };
+
+  handleCarouselNext = () => {
+    let curIndex = this.state.activeIndex,
+        totalNum = this.props.source.length;
+
+    this.prevIndex = curIndex;
+
+    if (curIndex == totalNum - 1) {
+      curIndex = 0;
+    } else {
+      curIndex += 1;
+    }
+
+    this.setState({
+      activeIndex: curIndex,
+    }, () => {
+      this.carousel.next();
+    });
   };
 
   render() {
-    const { visible, isFullscreen } = this.state;
+    let _this = this;
+    const { visible, isFullscreen, isDisableDengbi, isDisableFangda, isDisableSuoxiao } = this.state;
     const { source, dots, activeIndex, controller } = this.props;
     let contentWrapClass = classNames({
         'm-picture-preview-content-wrap': true,
@@ -208,6 +298,11 @@ class PicturePreview extends Component {
         'img-wrap': true,
         'img-wrap-size': !isFullscreen,
         'img-wrap-size-fullscreen': isFullscreen
+    });
+    let dengbiClass = classNames({
+        'iconfont': true,
+        'icon-dengbi': true,
+        'icon-disable': isDisableDengbi
     });
 
     return (
@@ -228,6 +323,7 @@ class PicturePreview extends Component {
               dots={dots}
               effect={"fade"}
               initialSlide={activeIndex}
+              afterChange={this.resetImg}
               ref={node => this.carousel = node}
             >
               {
@@ -235,12 +331,18 @@ class PicturePreview extends Component {
                   // TODO: 计算图片的原始尺寸
                   const naturalWidth = parseInt(each.size.split("*")[0]);
                   const naturalHeight = parseInt(each.size.split("*")[1]);
-                  const imgRatio = naturalWidth / naturalHeight;
-                  let adaptiveWH = getAdaptiveWH(naturalWidth, naturalHeight, isFullscreen);
+                  let aImg = getAdaptiveImg(naturalWidth, naturalHeight, isFullscreen);
+
+                  _this.imgs[index].naturalWidth = naturalWidth;
+                  _this.imgs[index].naturalHeight = naturalHeight;
+                  _this.imgs[index].adaptiveWidth = aImg.width;
+                  _this.imgs[index].adaptiveHeight = aImg.height;
+                  _this.imgs[index].scale = 1;
+                  _this.imgs[index].rotate = 0;
 
                   return (
                     <div key={index} className={imgWrapClass}>
-                      <img src={each.url} data-ratio={imgRatio} width={adaptiveWH.width} height={adaptiveWH.height}/>
+                      <img src={each.url} width={aImg.width} height={aImg.height}/>
                     </div>
                   );
                 })
@@ -251,19 +353,19 @@ class PicturePreview extends Component {
           <i className="iconfont icon-guanbi" onClick={this.handleOnClose}/>
 
           <div className="btn-left btn" style={{display: source.length > 1 ? "flex" : "none"}}>
-            <i className="iconfont icon-zuojiantou1" onClick={() => this.carousel.prev()}/>
+            <i className="iconfont icon-zuojiantou1" onClick={this.handleCarouselPrev}/>
           </div>
 
           <div className="btn-right btn" style={{display: source.length > 1 ? "flex" : "none"}}>
-            <i className="iconfont icon-youjiantou1" onClick={() => this.carousel.next()}/>
+            <i className="iconfont icon-youjiantou1" onClick={this.handleCarouselNext}/>
           </div>
 
           <div className={ctrlClass}>
-            <i className="iconfont icon-dengbi" onClick={this.handleDengbi}/>
+            <i className={dengbiClass} onClick={this.handleZoom.bind(this, this.state.activeIndex, '1:1')}/>
             <i className={fullscreenClass} onClick={this.handleFullscreen}/>
-            <i className="iconfont icon-fangda" onClick={this.handleZoom.bind(this, '.carousel-wrap .slick-current img', true)}/>
-            <i className="iconfont icon-suoxiao" onClick={this.handleZoom.bind(this, '.carousel-wrap .slick-current img', false)}/>
-            <i className="iconfont icon-xuanzhuan" onClick={this.handleRotate.bind(this, '.carousel-wrap .slick-current img')}/>
+            <i className="iconfont icon-fangda" onClick={this.handleZoom.bind(this, this.state.activeIndex, 'in')}/>
+            <i className="iconfont icon-suoxiao" onClick={this.handleZoom.bind(this, this.state.activeIndex, 'out')}/>
+            <i className="iconfont icon-xuanzhuan" onClick={this.handleRotate.bind(this, this.state.activeIndex, false)}/>
             <i className="iconfont icon-save" onClick={this.handleSave.bind(this, '.carousel-wrap .slick-current img', null)}/>
           </div>
         </div>
