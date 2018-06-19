1ee2dc63a689a938292f8ef179bca442389c7517
qingze <hzqingze@corp.netease.com>
2018-06-19 15:39:34 +0800

    路由修改

--- site/home.js
+++ site/home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Link} from 'react-router';
 
 export default class Home extends React.Component {
   constructor(props) {
@@ -17,13 +16,13 @@ export default class Home extends React.Component {
             </h1>
             <ul className="nav">
               <li className="nav-item">
-                <Link className="active" rel="noopener noreferrer">首页</Link>
+                <a className="active" rel="noopener noreferrer">首页</a>
               </li>
               <li className="nav-item">
-                <Link to="spec/" rel="noopener noreferrer">设计语言</Link>
+                <a href="#/spec/" rel="noopener noreferrer">设计语言</a>
               </li>
               <li className="nav-item">
-                <Link to="components/">组件</Link>
+                <a href="#/components/">组件</a>
               </li>
             </ul>
           </div>
