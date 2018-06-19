f9acb1fa66937eaa709e47d2d70951cdbab0ef82
zhiyou <gtopia@163.com>
2018-06-19 16:58:42 +0800

    修改 jest 配置

--- jest.config.js
+++ jest.config.js
@@ -3,16 +3,18 @@ module.exports = {
 	  "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/tools/assetsTransformer.js",
 	  "\\.(css|less)$": "<rootDir>/tools/assetsTransformer.js"
 	},
-	testMatch: [ "**/__tests__/**/*.js?(x)", "**/?(*.)+(test).js?(x)" ],
+	moduleFileExtensions: ["js"],
+	testMatch: [ "**/__tests__/**/*.js", "**/?(*.)+(test).js" ],
   verbose: true,
   notify: false,
   collectCoverage: false,
   collectCoverageFrom: [
-    "**/source/components/**/*.js",
-    "!**/source/components/**/demo/*.js",
-    "!**/node_modules/**",
-    "!**/tools/**",
-    "!**/doc/**"
+    "<rootDir>/source/components/**/*.js",
+    "!<rootDir>/source/components/**/demo/*.js",
+    "!<rootDir>/source/components/ReactAmap/components/utils/*.js",
+    "!<rootDir>/node_modules/**",
+    "!<rootDir>/tools/*",
+    "!<rootDir>/doc/*"
   ],
   coverageDirectory: '<rootDir>/coverage',
 };
