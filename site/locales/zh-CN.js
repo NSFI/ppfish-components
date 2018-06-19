ef177b3545546a4d6e5a842bcd78fe16c06e2c5e
and80506 <and80506@163.com>
2018-06-19 16:23:37 +0800

    增加设计资源页面

--- site/locales/zh-CN.js
+++ site/locales/zh-CN.js
@@ -1,20 +1,20 @@
 module.exports = {
   page: {
     'quick-start': '快速上手',
+    'changelog': '更新日志',
+    'contributing': '贡献指南',
+    'resource': '设计资源',
     'color': 'Color 色彩',
     'typography': 'Typography 字体',
     'icon': 'Icon 图标',
     'button': 'Button 按钮',
     'radio': 'Radio 单选框',
     'checkbox': 'Checkbox 多选框',
-    'contributing': '贡献指南',
-    'changelog': '更新日志',
     'animationImageLoader': '逐帧动画'
   },
   misc: {
     'guide': '指南',
     'component': '组件',
-    'resource': '资源',
     'development': '开发指南',
     'components': '基础组件',
     'feedback': '反馈建议',
