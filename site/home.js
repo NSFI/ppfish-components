3523eb841259ed447f6e32531371b3f7daab0544
and80506 <and80506@163.com>
2018-06-20 17:33:21 +0800

    优化layout；

--- site/home.js
+++ site/home.js
@@ -1,34 +1,16 @@
 import React from 'react';
+import Layout from './layout';
 
 export default class Home extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
   }
 
   render() {
     return (
-      <div className="app">
-        <header className="header">
-          <div className="container">
-            <h1>
-              LOGO
-            </h1>
-            <ul className="nav">
-              <li className="nav-item">
-                <a className="active" rel="noopener noreferrer">首页</a>
-              </li>
-              <li className="nav-item">
-                <a href="#/spec/" rel="noopener noreferrer">设计语言</a>
-              </li>
-              <li className="nav-item">
-                <a href="#/components/">组件</a>
-              </li>
-            </ul>
-          </div>
-        </header>
-        <div>首页</div>
-      </div>
+      <Layout>
+        首页
+      </Layout>
     );
   }
 }
--- site/spec.js
+++ site/spec.js
@@ -1,34 +1,16 @@
 import React from 'react';
+import Layout from './layout';
 
 export default class Spec extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
   }
 
   render() {
     return (
-      <div className="app">
-        <header className="header">
-          <div className="container">
-            <h1>
-              LOGO
-            </h1>
-            <ul className="nav">
-              <li className="nav-item">
-                <a href="#/home/" rel="noopener noreferrer">首页</a>
-              </li>
-              <li className="nav-item">
-                <a className="active" rel="noopener noreferrer">设计语言</a>
-              </li>
-              <li className="nav-item">
-                <a href="#/components/">组件</a>
-              </li>
-            </ul>
-          </div>
-        </header>
-        <div>设计语言</div>
-      </div>
+      <Layout>
+        设计语言
+      </Layout>
     );
   }
 }
