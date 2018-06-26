9bf87edda07cd5059c6538ec477c940e04e001f3
wangxiaoling <wangxiaoling@corp.netease.com>
2018-06-26 17:02:43 +0800

    业务组件迁移

--- site/pages/index.js
+++ site/pages/index.js
@@ -5,7 +5,7 @@ export default {
     'changelog': require('./changelog'),
     'contributing': require('./contributing'),
     'resource': require('./resource'),
-    'customTheme': require('./customTheme'),
+    'CustomTheme': require('./CustomTheme'),
   },
   components: {
     '通用组件': {
@@ -14,7 +14,22 @@ export default {
       'button': require('./button'),
       'dropdown': require('./dropdown'),
       'loading': require('./loading'),
-
+    },
+    '业务组件': {
+      'Collapse': require('./Collapse'),
+      'CustomTable': require('./CustomTable'),
+      'DayTimeSelect': require('./DayTimeSelect'),
+      'Drawer': require('./Drawer'),
+      'Echart': require('./Echart'),
+      'ImageLoader': require('./ImageLoader'),
+      'NumberCounter': require('./NumberCounter'),
+      'PicturePreview': require('./PicturePreview'),
+      'ReactAmap': require('./ReactAmap'),
+      'SearchInput': require('./SearchInput'),
+      'StickVerticalMenu': require('./StickVerticalMenu'),
+      'TextOverFlow': require('./TextOverFlow'),
+      'TimePicker': require('./TimePicker'),
+      'TreeSelect': require('./TreeSelect'),
     }
   }
 };
