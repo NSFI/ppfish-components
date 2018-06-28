aff6c107ebc73ab4495f5e30322c3dd455c98c6a
andy wu <wushengzhu114@gmail.com>
2018-06-28 19:40:51 +0800

    干掉冗余代码；

--- source/config/index.js
+++
@@ -1,8 +0,0 @@
-import * as dev from './dev';
-import * as prod from './prod';
-export const getConfig = () => {
-  if (process.env.NODE_ENV === 'production') {
-    return prod;
-  }
-  return dev;
-};
--- source/utils/webpackPublicPath.js
+++
@@ -1,4 +0,0 @@
-// configuring webpack public path at runtime
-// __webpack_public_path__ = __webpack_public_path__;
-// eslint-disable-next-line no-undef
-__webpack_public_path__ = window.location.protocol + "//" + window.location.host + "/";
