e8f24033f58193c8ecde9ef8fe8e85ee5da5634b
yuwen <yuwen1@corp.netease.com>
2018-07-05 11:52:10 +0800

    修复: 颜色 字体页面 class命名

--- site/componentsPage/ruleText/index.js
+++ site/componentsPage/ruleText/index.js
@@ -13,56 +13,56 @@ export default class RuleText extends React.Component {
 
   render() {
     return (
-      <div>
+      <div id='typography-text'>
         <h1 className='global-title'>Typography 字体</h1>
-        <p>文字色主要用于各层级的文本来使用。</p>
+        <p className='desc'>文字色主要用于各层级的文本来使用。</p>
         
         <h3 id='text-color-title'>文字色</h3>
-        <div className='text-color'>
-          <div className='text-color-item palatte-1' onClick={() => {this.handleClick('#333333')}}>
+        <div className='text-color-container'>
+          <div className='text-color-item' onClick={() => {this.handleClick('#333333')}}>
             #3 Black
             <div className='value'>#333333</div>
           </div>
-          <div className='text-color-item palatte-2' onClick={() => {this.handleClick('#666666')}}>
+          <div className='text-color-item' onClick={() => {this.handleClick('#666666')}}>
             #6 Black
             <div className='value'>#666666</div>
           </div>
-          <div className='text-color-item palatte-3' onClick={() => {this.handleClick('#999999')}}>
+          <div className='text-color-item' onClick={() => {this.handleClick('#999999')}}>
             #9 Black
             <div className='value'>#999999</div>
           </div>
-          <div className='text-color-item palatte-4' onClick={()  => {this.handleClick('#CCCCCC')}}>
+          <div className='text-color-item' onClick={()  => {this.handleClick('#CCCCCC')}}>
             #C Black
             <div className='value'>#CCCCCC</div>
           </div>
         </div>
 
-        <h3 id='chinese-text-title'>中文字体</h3>
-        <div className='chinese-text-container'>
-          <div className='chinese-text-item type-PingFang'>
+        <h3 id='ch-font-title'>中文字体</h3>
+        <div className='ch-font-container'>
+          <div className='font-item font-PingFang'>
             苹方
             <div className='name'>PingFang SC</div>
           </div>
-          <div className='chinese-text-item type-Microsoft'>
+          <div className='font-item font-Microsoft'>
             微软雅黑
             <div className='name'>Microsoft YaHei</div>
           </div>
         </div>
 
-        <h3 id='eng-text-title'>英文字体</h3>
-        <div className='eng-text-container'>
-          <div className='eng-text-item type-Arial'>
+        <h3 id='eng-font-title'>英文字体</h3>
+        <div className='eng-font-container'>
+          <div className='font-item font-Arial'>
             Arial
             <div className='name'>Arial</div>
           </div>
-          <div className='eng-text-item type-Helvetica'>
+          <div className='font-item font-Helvetica'>
             Helvetica Neue
             <div className='name'>Helvetica Neue</div>
           </div>
         </div>
 
         <h3 id='text-rule-title'>字体使用规范</h3>
-        <table>
+        <table className='text-rule-table'>
           <tbody>
             <tr>
               <td className='h1'>页面标题</td>
