de2d7461b244fde63728d25df35937144c2a4c84
zhiyou <gtopia@163.com>
2018-07-05 15:42:41 +0800

    定制编辑器字体大小

--- source/components/RichEditor/index.js
+++ source/components/RichEditor/index.js
@@ -7,127 +7,164 @@ import './index.less';
 
 console.log(Quill.imports);
 
+// 自定义模块
 var Module = Quill.import('core/module');
 class CustomModule extends Module {}
-Quill.register('modules/custom-module', CustomModule);
+// Quill.register('modules/custom-module', CustomModule);
 
 
+// 字体
 var FontAttributor = Quill.import('attributors/class/font');
 FontAttributor.whitelist = [
   'sofia', 'slabo', 'roboto', 'inconsolata', 'ubuntu'
 ];
-Quill.register(FontAttributor, true);
+// Quill.register(FontAttributor, true);
 
 
+// 背景色，文字色，字体样式
 var BackgroundClass = Quill.import('attributors/class/background');
 var ColorClass = Quill.import('attributors/class/color');
 var SizeStyle = Quill.import('attributors/style/size');
-Quill.register(BackgroundClass, true);
-Quill.register(ColorClass, true);
-Quill.register(SizeStyle, true);
+// Quill.register(BackgroundClass, true);
+// Quill.register(ColorClass, true);
+// Quill.register(SizeStyle, true);
 
 
+// 字体加粗
 var Bold = Quill.import('formats/bold');
 Bold.tagName = 'B';   // 自定义使用的HTML标签，Quill uses <strong> by default
-Quill.register(Bold, true);
+// Quill.register(Bold, true);
 
 
+// 插入表情
 let Inline = Quill.import('blots/inline');
 class EmojiBlot extends Inline { }
 EmojiBlot.blotName = 'emoji';
 EmojiBlot.tagName = 'img';
 Quill.register(EmojiBlot);
 
-class SizeBlot extends Inline {
+
+// 自定义字体大小
+class CustomSizeBlot extends Inline {
   static create(value) {
     let node = super.create();
-    // node.setAttribute('font-size', value);
     node.style.fontSize = value;
-    // debugger;
     return node;
   }
 
-  static formats(node, tmp) {
+  static formats(node) {
     return node.style.fontSize;
-    // debugger;
-    // return SizeBlot.tagName.indexOf(node.tagName) + 1;
+  }
+
+  static value(node) {
+    return node.style.fontSize;
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.defaultValue = '14px';
+  }
+
+  format(name, value) {
+    if (name === 'customSize') {
+      if (value) {
+        this.domNode.style.fontSize = value;
+      } else {
+        this.domNode.style.fontSize = this.defaultValue;
+      }
+    } else {
+      super.format(name, value);
+    }
   }
 }
-SizeBlot.blotName = 'size';
-SizeBlot.tagName = 'span';
-Quill.register(SizeBlot);
+CustomSizeBlot.blotName = 'customSize';
+CustomSizeBlot.tagName = 'span';
+Quill.register(CustomSizeBlot);
 
-const CustomButton = () => <span className="octicon octicon-star" />;
-function insertStar() {
-  const cursorPosition = this.quill.getSelection().index;
-  this.quill.insertText(cursorPosition, "★");
-  this.quill.setSelection(cursorPosition + 1);
-}
 
-const CustomToolbar = () => (
-  <div id="toolbar">
-    <div className="m-rich-editor-grp">
-      <button className="ql-link" />
-      <button className="ql-bold" />
-      <button className="ql-italic" />
-      <button className="ql-underline" />
+const CustomToolbar = (props) => {
+  let showSizePanel = () => {
+    // debugger;
+  };
+
+  return (
+    <div id="toolbar" className={props.className}>
+      <div className="toolbar-grp">
+        <button className="item ql-link" />
+        <button className="item ql-bold" />
+        <button className="item ql-italic" />
+        <button className="item ql-underline" />
+      </div>
+
+      <div className="toolbar-grp">
+        <select className="item ql-color">
+        {
+          /*
+          <option value="red" />
+          <option value="green" />
+          <option value="blue" />
+          <option value="orange" />
+          <option value="violet" />
+          <option value="#d0d1d2" />
+          <option value="black" />        
+           */
+        }
+        </select>
+      </div>
+
+      <div className="toolbar-grp">
+        <select className="item ql-align"></select>
+        {
+          /*
+        <button className="ql-align" />
+        <button className="ql-align" value="center" />
+        <button className="ql-align" value="right" />
+           */
+        }
+      </div>
+
+      <div className="toolbar-grp">
+        <button type="button" className="item ql-list" value="ordered" />
+        <button type="button" className="item ql-list" value="bullet" />
+      </div>
+
+      <div className="toolbar-grp">
+        <button className="item ql-emoji" />
+      </div>
+
+      <div className="toolbar-grp">
+        <button className="item ql-image" />
+      </div>
+
+      <div className="toolbar-grp">
+        <div className="item custom-size" onClick={showSizePanel}></div>
+        <div className="custom-size-panel hide">
+          <button type="button" className="ql-customSize item" value="32px">32px</button>
+          <button type="button" className="ql-customSize item" value="24px">24px</button>
+          <button type="button" className="ql-customSize item" value="18px">18px</button>
+          <button type="button" className="ql-customSize item" value="16px">16px</button>
+          <button type="button" className="ql-customSize item" value="13px">13px</button>
+          <button type="button" className="ql-customSize item" value="12px">12px</button>
+        </div>
+      </div>
+
+      <div className="toolbar-grp">
+        <button className="item ql-clean" />
+      </div>
+
+      <div className="toolbar-grp">
+        <button className="item ql-entry" />
+      </div>
+
+      <div className="toolbar-grp">
+        <button className="item ql-insertStar">
+          <span className="octicon octicon-star" />
+        </button>
+      </div>
     </div>
-
-    <div className="m-rich-editor-grp">
-      <select className="ql-color">
-        <option value="red" />
-        <option value="green" />
-        <option value="blue" />
-        <option value="orange" />
-        <option value="violet" />
-        <option value="#d0d1d2" />
-        <option value="black" />
-      </select>
-    </div>
-
-    <div className="m-rich-editor-grp">
-      <button className="ql-align" />
-      <button className="ql-align" value="center" />
-      <button className="ql-align" value="right" />
-    </div>
-
-    <div className="m-rich-editor-grp">
-      <button type="button" className="ql-list" value="ordered" />
-      <button type="button" className="ql-list" value="bullet" />
-    </div>
-
-    <div className="m-rich-editor-grp">
-      <button className="ql-emoji" />
-    </div>
-
-    <div className="m-rich-editor-grp">
-      <button className="ql-image" />
-    </div>
-
-    <div className="m-rich-editor-grp">
-      <select className="ql-size" defaultValue={""} onChange={e => e.persist()}>
-        <option value="32px">32px</option>
-        <option value="24px">24px</option>
-        <option value="18px">18px</option>
-        <option value="16px">16px</option>
-        <option value="13px">13px</option>
-        <option value="12px">12px</option>
-      </select>
-    </div>
-
-    <div className="m-rich-editor-grp">
-      <button className="ql-clean" />
-    </div>
-
-    <div className="m-rich-editor-grp">
-      <button className="ql-entry" />
-    </div>
-
-    <button className="ql-insertStar">
-      <CustomButton />
-    </button>
-  </div>
-);
+  );
+};
 
 class RichEditor extends Component {
   static propTypes = {
@@ -140,26 +177,17 @@ class RichEditor extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { text: 'asdfadsf' }; // You can also pass a Quill Delta here
-    // this.modules = {
-    //   toolbar: [
-    //     ['link', 'bold', 'italic', 'underline'],
-    //     ['color'],
-    //     [{'list': 'ordered'}, {'list': 'bullet'}],
-    //     // ['emoji'],
-    //     ['image'],
-    //     ['size'],
-    //     ['clean'],
-    //     // ['entry'],
-        // [{'header': [1, 2, 3, false] }],
-    //   ],
-    // };
 
+    this.state = { text: '初始内容a<br/>初始内容a' };
     this.modules = {
       toolbar: {
         container: "#toolbar",
         handlers: {
-          'insertStar': insertStar,
+          'insertStar': function insertStar() {
+            const cursorPosition = this.quill.getSelection().index;
+            this.quill.insertText(cursorPosition, "★");
+            this.quill.setSelection(cursorPosition + 1);
+          },
           'link': function(value) {
             if (value) {
               var href = prompt('Enter the URL');
@@ -168,31 +196,14 @@ class RichEditor extends Component {
               this.quill.format('link', false);
             }
           },
-          // 'size': function(value) {
-          //   this.quill.format('size', value);
+          // 'customSize': function(value) {
+          //   let sec = this.quill.getSelection();
           //   debugger;
+          //   this.quill.format('customSize', value);
           // }
         }
       }
     };
-
-    // this.formats = [
-    //   'link', 'bold', 'italic', 'underline', 
-    //   'color', 'size',
-    //   'list',
-    //   'bullet',
-    //   'image',
-    //   'header',
-    //   'clean',
-    //   // 'emoji',
-    //   // 'entry',
-    //   'indent',
-    //   "font",
-    //   "strike",
-    //   "blockquote",
-    // ];
-
-    this.formats = ['italic', 'underline'];
   }
 
   handleChange = (value) => {
@@ -201,14 +212,13 @@ class RichEditor extends Component {
 
   render() {
     return (
-      <div className="text-editor">
-        <CustomToolbar />
+      <div className="m-rich-editor">
+        <CustomToolbar className={'editor-head'}/>
         <ReactQuill
-          className={'testclass testclass2'}
+          className={'editor-body'}
           value={this.state.text}
           placeholder={'placeholder'}
           modules={this.modules}
-          // formats={this.formats}
           onChange={this.handleChange}
         />
       </div>
