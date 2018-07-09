fc9a62e4b54da3de41127063e6ee208cd4b5a66c
zhiyou <gtopia@163.com>
2018-07-09 21:10:01 +0800

    添加编辑器表情

--- source/components/RichEditor/emojiList.js
+++ source/components/RichEditor/emojiList.js
@@ -9,6 +9,48 @@ let emojiList = [
     name: '00',
     title: '[大笑]'
   },
+  {
+    id: 2,
+    name: '02',
+    title: '[色]'
+  },
+  {
+    id: 3,
+    name: '03',
+    title: '[嘘]'
+  },
+  {
+    id: 4,
+    name: '04',
+    title: '[亲]'
+  },
+  {
+    id: 5,
+    name: '05',
+    title: '[呆]'
+  },
+  {
+    id: 6,
+    name: '06',
+    title: '[口水]'
+  },
+  {
+    id: 8,
+    name: '07',
+    title: '[呲牙]'
+  },
+  {
+    id: 9,
+    name: '08',
+    title: '[鬼脸]'
+  },
+  {
+    id: 10,
+    name: '09',
+    title: '[害羞]'
+  },
+
+
 ];
 
 export default emojiList;
