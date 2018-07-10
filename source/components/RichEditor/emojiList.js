d1a2ccab88dd6d9cecd3cac1f13e9e2709e3a79c
zhiyou <gtopia@163.com>
2018-07-10 17:26:18 +0800

    补充emoji表情

--- source/components/RichEditor/emojiList.js
+++ source/components/RichEditor/emojiList.js
@@ -35,6 +35,11 @@ let emojiList = [
     title: '[口水]'
   },
   {
+    id: 7,
+    name: '145',
+    title: '[汗]'
+  },
+  {
     id: 8,
     name: '07',
     title: '[呲牙]'
@@ -48,6 +53,201 @@ let emojiList = [
     id: 10,
     name: '09',
     title: '[害羞]'
+  },
+  {
+    id: 11,
+    name: '10',
+    title: '[偷笑]'
+  },
+  {
+    id: 12,
+    name: '11',
+    title: '[调皮]'
+  },
+  {
+    id: 13,
+    name: '12',
+    title: '[可怜]'
+  },
+  {
+    id: 14,
+    name: '13',
+    title: '[敲]'
+  },
+  {
+    id: 15,
+    name: '14',
+    title: '[惊讶]'
+  },
+  {
+    id: 16,
+    name: '15',
+    title: '[流感]'
+  },
+  {
+    id: 17,
+    name: '16',
+    title: '[委屈]'
+  },
+  {
+    id: 18,
+    name: '17',
+    title: '[流泪]'
+  },
+  {
+    id: 19,
+    name: '18',
+    title: '[嚎哭]'
+  },
+  {
+    id: 20,
+    name: '19',
+    title: '[惊恐]'
+  },
+  {
+    id: 21,
+    name: '20',
+    title: '[怒]'
+  },
+  {
+    id: 22,
+    name: '21',
+    title: '[酷]'
+  },
+  {
+    id: 23,
+    name: '22',
+    title: '[不说]'
+  },
+  {
+    id: 24,
+    name: '23',
+    title: '[鄙视]'
+  },
+  {
+    id: 25,
+    name: '24',
+    title: '[阿弥陀佛]'
+  },
+  {
+    id: 26,
+    name: '25',
+    title: '[奸笑]'
+  },
+  {
+    id: 27,
+    name: '26',
+    title: '[睡着]'
+  },
+  {
+    id: 28,
+    name: '27',
+    title: '[口罩]'
+  },
+  {
+    id: 29,
+    name: '28',
+    title: '[生气]'
+  },
+  {
+    id: 30,
+    name: '29',
+    title: '[抠鼻孔]'
+  },
+  {
+    id: 31,
+    name: '30',
+    title: '[疑问]'
+  },
+  {
+    id: 32,
+    name: '31',
+    title: '[怒骂]'
+  },
+  {
+    id: 33,
+    name: '32',
+    title: '[晕]'
+  },
+  {
+    id: 34,
+    name: '33',
+    title: '[呕吐]'
+  },
+  {
+    id: 35,
+    name: '160',
+    title: '[拜一拜]'
+  },
+  {
+    id: 36,
+    name: '161',
+    title: '[惊喜]'
+  },
+  {
+    id: 37,
+    name: '162',
+    title: '[流汗]'
+  },
+  {
+    id: 38,
+    name: '163',
+    title: '[卖萌]'
+  },
+  {
+    id: 39,
+    name: '164',
+    title: '[默契眨眼]'
+  },
+  {
+    id: 40,
+    name: '165',
+    title: '[烧香拜佛]'
+  },
+  {
+    id: 41,
+    name: '166',
+    title: '[晚安]'
+  },
+  {
+    id: 42,
+    name: '116',
+    title: '[撇嘴]'
+  },
+  {
+    id: 43,
+    name: '117',
+    title: '[难过]'
+  },
+  {
+    id: 44,
+    name: '118',
+    title: '[冷汗]'
+  },
+  {
+    id: 45,
+    name: '119',
+    title: '[抓狂]'
+  },
+  {
+    id: 46,
+    name: '120',
+    title: '[傲慢]'
+  },
+  {
+    id: 47,
+    name: '121',
+    title: '[困]'
+  },
+  {
+    id: 48,
+    name: '123',
+    title: '[疯了]'
+  },
+  {
+    id: 49,
+    name: '124',
+    title: '[白眼]'
   },
 
 
