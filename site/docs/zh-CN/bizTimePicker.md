63b098338ba4d3a3112923f7bfb76b3290e5f4b2
wangxiaoling <wangxiaoling@corp.netease.com>
2018-07-16 16:15:51 +0800

    site路由路径写错了

--- site/docs/zh-CN/bizTimePicker.md
+++ site/docs/zh-CN/bizTimePicker.md
@@ -18,7 +18,6 @@
     return (
       <BizTimePicker
         clickAreaStyle={{boxShadow:"none",border:"1px solid #d4d6d9"}}
-        dateFormat="YYYY-MM-DD"
         onChange={this.handleTimeChange}
       />
     );
@@ -26,92 +25,19 @@
 ```
 :::
 
-## 自定义快速选择时间
-
-:::demo 自定义快速选择时间。
-```js
-  handleTimeChange = (time) => {
-    console.log(time);
-  }
-
-  render() {
-    return (
-      <BizTimePicker
-        clickAreaStyle={{boxShadow:"none",border:"1px solid #d4d6d9"}}
-        quickTimeOption={[
-                           { text: '前天', value: 3, endDate: '今天' },
-                           { text: '过去一个月', value: 60, endDate: '今天' },
-                        ]}
-        onChange={this.handleTimeChange}
-      />
-    );
-  }
-```
-:::
-
-## 设置最大可选择的日期范围
-
-:::demo 设置最大可选择的日期范围。当选择范围超过设置值时，将会报错提示。
-```js
-  handleTimeChange = (time) => {
-    console.log(time);
-  }
-
-  render() {
-    return (
-      <BizTimePicker
-        clickAreaStyle={{boxShadow:"none",border:"1px solid #d4d6d9"}}
-        maxDateRange={31}
-        onChange={this.handleTimeChange}
-      />
-    );
-  }
-```
-:::
-
-## 不可选择的日期
-
-:::demo 设置不可选择日期。
-```js
-  disabledDate = (current) => {
-    //2017年1月1号之前的不能选
-    const disableDay = new Date("2017/01/01").setHours(0, 0, 0, 0);
-    if (current.valueOf() <= disableDay) {
-      return true;
-    }
-    //今天以及将来的时间不能选
-    let day = (current.valueOf() - Date.now()) / 86400000;
-    return current && day > 0 || Math.abs(day) < 1;
-  };
-
-  handleTimeChange = (time) => {
-    console.log(time);
-  }
-
-  render() {
-    return (
-      <BizTimePicker
-        clickAreaStyle={{boxShadow:"none",border:"1px solid #d4d6d9"}}
-        disabledDate={this.disabledDate}
-        onChange={this.handleTimeChange}
-      />
-    );
-  }
-```
-:::
 
 
 
 ## API
 | 属性      | 说明    | 类型      |  默认值   |
 |---------- |-------- |---------- |-------- |
+| clickAreaStyle  | 点击区域的样式    | object  | - |
+| defaultTime  | 默认时间，支持两种格式，例如：{ text: '昨天', value: 1, endDate: '昨天' } 或 [startTime, endTime] | array/object   | quickTimeOption[0]   |
 | quickTimeOption | 快速选择时间选项（格式见默认值：text代表显示文本，endDate代表结束日期，可选择的有"今天"、"昨天"、"前天"，value代表到结束日期的天数） | array | [{ text: '昨天', value: 1, endDate: '昨天' },{ text: '过去7天', value: 7, endDate: '昨天' },{ text: '过去30天', value: 30, endDate: '昨天' }]|
 | maxDateRange  | 最大可选择的日期范围，单位 天    | number   | null   |
 | dateFormat  | 日期展示格式 | string   | "YYYY/MM/DD"   |
 | allowClear  | 	是否显示日历的清除按钮    | bool   | false   |
 | visible  | 用于手动控制浮层显隐    | bool   | false   |
-| defaultTime  | 默认时间，支持两种格式，例如：{ text: '昨天', value: 1, endDate: '昨天' } 或 [startTime, endTime] | array/object   | quickTimeOption[0]   |
-| clickAreaStyle  | 点击区域的样式    | object  | - |
 | disabledDate  | 不可选择的日期    | (currentDate: moment) => boolean   | - |
 | onChange  | 时间发生变化的回调   | function([startTime,endTime], quickTimeOptionItem)   | - |
 | onOpenChange  | 弹出或关闭浮层的回调 | function(status)   | - |
--- source/components/BizTimePicker/index.js
+++ source/components/BizTimePicker/index.js
@@ -1,59 +1,81 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import { DatePicker, Popover, message } from 'antd';
-import { formatTimestamp, getTimeFromTimestamp } from '../../utils';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
 
 import './index.less';
 
 const { RangePicker } = DatePicker;
 
-//默认快速选择时间选项
+// 默认快速选择时间选项
 const defaultQuickTimeOption = [
-  { text: '昨天', value: 1, endDate: '昨天' },
-  { text: '过去7天', value: 7, endDate: '昨天' },
-  { text: '过去30天', value: 30, endDate: '昨天' },
+  { text: '今天', value: [moment(), moment()] },
+  { text: '这个月', value: [moment(), moment().endOf('month')] },
 ];
 
-//默认时间传进来有两种格式：{ text: '昨天', value: 1, endDate: '昨天' } 或 [startTime, endTime]
-const formatDefaultTime = (param) => {
-  let result = { text: null, value: null };
-  if(param instanceof Array){
-    if(param.length === 2){
-      const time = getTimeFromTimestamp(param[0],param[1]);
-      result = { text: time, value: time };
-    }
-  }else if(param instanceof Object && 'text' in param && 'value' in param){
-    result = param;
+// 默认时间传进来有两种格式：{ text: '今天', value: [moment(), moment()] } 或 [moment, moment]
+const getValueFromDefaultTime = (defaultValue) => {
+  let result = [];
+  if(Array.isArray(defaultValue) && defaultValue.length === 2) {
+    result = defaultValue;
+  }else if(defaultValue instanceof Object && 'value' in defaultValue) {
+    result = defaultValue.value;
   }
   return result;
 };
 
+const getShowDateFromValue = (value) => {
+  let result = { text: null, value: null };
+  if(value instanceof Object && 'text' in value && 'value' in value) {
+    result = value;
+  }else if(Array.isArray(value) && value.length === 2) {
+    result = { text: value, value: value}
+  }
+  return result;
+};
+
+// const getShowDateFromValue = (value) => {
+//   const [start, end] = value;
+//   // value could be an empty array, then we should not reset showDate
+//   if (!start && !end) {
+//     return;
+//   }
+//   const newEnd = end && end.isSame(start, 'month') ? end.clone().add(1, 'month') : end;
+//   return [start, newEnd];
+// };
+
 class BizTimePicker extends React.Component {
 
   static propTypes = {
+    prefixCls: PropTypes.string,         //前缀
+    clickAreaStyle: PropTypes.object,   //点击区域的样式
+    defaultValue: PropTypes.oneOfType([  //默认时间
+      PropTypes.array,
+      PropTypes.object,
+    ]),
+    value: PropTypes.array,              //value值
     quickTimeOption: PropTypes.array,  //快速选择时间选项
     maxDateRange: PropTypes.number,    //最大可选择的日期范围，单位 天
     dateFormat: PropTypes.string,      //日期展示格式
     allowClear: PropTypes.bool,        //是否显示日历的清除按钮
-    visible: PropTypes.bool,           //用于手动控制浮层显隐
-    defaultTime: PropTypes.oneOfType([  //默认时间
-      PropTypes.array,
-      PropTypes.object,
-    ]),
-    clickAreaStyle: PropTypes.object,   //点击区域的样式
+    open: PropTypes.bool,              //用于手动控制浮层显隐
     disabledDate: PropTypes.func,       //不可选择的日期
     onChange: PropTypes.func,           //时间发生变化的回调
     onOpenChange: PropTypes.func,       //弹出或关闭浮层的回调
   }
 
   static defaultProps = {
+    prefixCls: 'biz-time-picker',
+    clickAreaStyle: null,
+    defaultValue: defaultQuickTimeOption[0],
+    value: null,
     quickTimeOption: defaultQuickTimeOption,
     maxDateRange: null,
     dateFormat: "YYYY/MM/DD",
     allowClear: false,
-    visible: false,
-    defaultTime: null,
-    clickAreaStyle: null,
+    open: false,
     disabledDate: () => {},
     onChange: () => {},
     onOpenChange: ()=>{},
@@ -61,30 +83,42 @@ class BizTimePicker extends React.Component {
 
   constructor(props) {
     super(props);
+    const value = props.value || getValueFromDefaultTime(props.defaultValue) || [];
+    if( value[0] && !moment.isMoment(value[0]) || !value[1] && moment.isMoment(value[1]) ) {
+      throw new Error('The value/defaultValue of BizTimePicker must be a moment object array');
+    }
     this.state = {
-      currentTime: this.props.defaultTime ? formatDefaultTime(this.props.defaultTime) : formatDefaultTime(this.props.quickTimeOption[0]),
-      visible: false,
+      value,
+      showDate: getShowDateFromValue(props.value || props.defaultValue || []),
+      open: props.open,
     };
   }
 
   componentWillReceiveProps(nextProps) {
-    if(this.props.visible !== nextProps.visible){
+    if ('value' in nextProps) {
+      const value = nextProps.value || [];
       this.setState({
-        visible: nextProps.visible,
+        value,
+        showDate: getShowDateFromValue(value) || this.state.showDate,
+      });
+    }
+    if('open' in nextProps && this.props.open != nextProps.open){
+      this.setState({
+        open: nextProps.open,
       });
     }
   }
 
   handleQickTime(item) {
     this.setState({
-      currentTime: item,
-      visible: false,
+      showDate: item,
+      open: false,
     }, () => {
-      this.props.onChange(formatTimestamp(item), item);
+      this.props.onChange(item);
     });
   }
 
-  handleCustomerTime(date, dateString) {
+  handleCustomerTime = (date, dateString) => {
     const maxRange = this.props.maxDateRange;
     if(maxRange && maxRange > 0) {
       if(date.length === 2 && (date[1].valueOf() - date[0].valueOf()) / 86400000 + 1 > maxRange){
@@ -94,39 +128,49 @@ class BizTimePicker extends React.Component {
     }
     const timeString = dateString[0] + ' ~ ' + dateString[1];
     this.setState({
-      currentTime: {
+      showDate: {
         text: timeString,
         value: timeString
       },
-      visible: false,
+      open: false,
     }, () => {
-      this.props.onChange(formatTimestamp(this.state.currentTime.value));
+      this.props.onChange(this.state.showDate);
     });
   }
 
-  handleVisibleChange = (visible) => {
-    this.setState({ visible },()=>{
-      this.props.onOpenChange(this.state.visible);
+  handleVisibleChange = (open) => {
+    this.setState({ open },()=>{
+      this.props.onOpenChange(this.state.open);
     });
   }
 
   render() {
-    const {quickTimeOption, dateFormat, allowClear, disabledDate, clickAreaStyle} = this.props;
-    const {currentTime, visible} = this.state;
+    const {
+      prefixCls,
+      quickTimeOption,
+      dateFormat,
+      allowClear,
+      disabledDate,
+      clickAreaStyle,
+    } = this.props;
+    const { showDate, open, value } = this.state;
 
-    const prefixCls = 'time-picker';
+    const clickAreaIconClass = classNames({
+      [`${prefixCls}-click-area-icon`]: true,
+      'icon-active': open
+    });
 
     const content = (
       <div className={`${prefixCls}-content`}>
         {
           quickTimeOption.map((item, index) =>
             <a
-              className={(item.text === currentTime.text)? "quick-picker-item quick-picker-item-active" : "quick-picker-item"}
+              className={(item.text === showDate.text) ? "quick-picker-item quick-picker-item-active" : "quick-picker-item"}
               key={`quick-item-${index}`}
               onClick={this.handleQickTime.bind(this, item)}>
               <span>{item.text}</span>
               {
-                item.text === currentTime.text ? <i className="iconfont icon-duigou" /> : null
+                item.text === showDate.text ? <i className="iconfont icon-duigou" /> : null
               }
             </a>
           )
@@ -138,7 +182,7 @@ class BizTimePicker extends React.Component {
             disabledDate={disabledDate}
             format={dateFormat}
             getCalendarContainer={()=>this.refs['time-picker-customer-time']}
-            onChange={this.handleCustomerTime.bind(this)}
+            onChange={this.handleCustomerTime}
           />
         </div>
       </div>
@@ -150,13 +194,13 @@ class BizTimePicker extends React.Component {
           placement="bottomLeft"
           content={content}
           trigger="click"
-          visible={visible}
+          visible={open}
           onVisibleChange={this.handleVisibleChange}
           getPopupContainer={()=>this.refs['time-picker-container']}
         >
           <div className={`${prefixCls}-click-area`} style={clickAreaStyle}>
-            <span className={`${prefixCls}-click-area-text`}>{currentTime.text}</span>
-            <span className={visible ? "time-picker-click-area-icon icon-active" : "time-picker-click-area-icon"} >
+            <span className={`${prefixCls}-click-area-text`}>{showDate.text}</span>
+            <span className={clickAreaIconClass} >
               <i className="iconfont icon-xiajiantou" />
             </span>
           </div>
