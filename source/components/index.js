1815abc423a6f3af904ea8f01e9ca2c6bf27bd25
zhiyou <gtopia@163.com>
2018-07-23 19:37:06 +0800

    TreeSelect2改为TreeSelect

--- source/components/index.js
+++ source/components/index.js
@@ -22,7 +22,7 @@ import StickVerticalMenu from './StickVerticalMenu';
 import Tabs from './Tabs';
 import TextOverflow from './TextOverflow';
 import Tooltip from './Tooltip/index.tsx';
-import TreeSelect from './TreeSelect2/index.tsx';
+import TreeSelect from './TreeSelect/index.tsx';
 import VideoViewer from './VideoViewer';
 import Echart from './EChart';
 
