d92a6713fb71da4f6ac891c67c38a520208ae550
qingze <hzqingze@corp.netease.com>
2018-07-24 17:57:45 +0800

    dropdown class

--- source/components/Dropdown/index.tsx
+++ source/components/Dropdown/index.tsx
@@ -1,8 +1,9 @@
 import Dropdown from './dropdown';
 import DropdownButton from './dropdown-button';
+import './style/index.less';
 
-export { DropDownProps } from './dropdown';
-export { DropdownButtonProps } from './dropdown-button';
+export {DropDownProps} from './dropdown';
+export {DropdownButtonProps} from './dropdown-button';
 
 Dropdown.Button = DropdownButton;
 export default Dropdown;
