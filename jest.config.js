a8e7c5527f36dcb971318fb257a40b13ce133fa8
eynol <625608852@qq.com>
2018-07-25 19:20:29 +0800

    setup(jest+typescript): 修改jest配置，使测试支持typescript

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,32 @@ module.exports = {
   setupFiles: [
     '<rootDir>/source/tests/setup.js',
   ],
-	moduleNameMapper: {
-	  "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/tools/assetsTransformer.js",
-	  "\\.(css|less)$": "<rootDir>/tools/assetsTransformer.js"
-	},
-	moduleFileExtensions: ["js"],
-	testMatch: [ "**/__tests__/**/*.js", "**/?(*.)+(test).js" ],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/tools/assetsTransformer.js",
+    "\\.(css|less)$": "<rootDir>/tools/assetsTransformer.js"
+  },
+  moduleFileExtensions: [
+    "ts", "tsx", "js"
+  ],
+  transform: {
+    "^.+\\.js$": "babel-jest",
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  globals: {
+    "ts-jest": {
+      "tsConfigFile": "tsconfig.json",
+      "enableTsDiagnostics": true,
+
+    },
+    "NODE_ENV": "test"
+  },
+  testMatch: [
+    "**/__tests__/**/*.(ts|tsx|js)",
+    "**/?(*.)+(test).(ts|tsx|js)"
+  ],
+  testPathIgnorePatterns: [
+      "<rootDir>/node_modules/",
+  ],
   verbose: true,
   notify: false,
   collectCoverage: false,
