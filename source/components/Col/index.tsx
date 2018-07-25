f3ec96c7ad3c107eee15196305a420fe9b54bfd5
qingze <hzqingze@corp.netease.com>
2018-07-25 11:25:06 +0800

    搬运Grid/Row/Col 替换Select中的ROW/COL

---
+++ source/components/Col/index.tsx
@@ -0,0 +1,6 @@
+import {Col, ColProps, ColSize} from '../Grid';
+import '../Grid/style/index.less';
+
+export {ColProps, ColSize};
+
+export default Col;
---
+++ source/components/Row/index.tsx
@@ -0,0 +1,5 @@
+import {Row, RowProps} from '../Grid';
+import '../Grid/style/index.less';
+
+export {RowProps};
+export default Row;
