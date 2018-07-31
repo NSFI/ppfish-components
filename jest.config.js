673770c2c81341c2b5f2b7ccd881df4c07b9f00a
eynol <625608852@qq.com>
2018-07-31 13:09:54 +0800

    package.json加入四个依赖；添加cross-env,开启babelrc测试环境使用dynamic-import-node(这个是markdown组件里面使用了import,后面可以考虑不要)；修复treePane重复定义；还原原来的测试用例。修改raf模块的引入方式。

--- jest.config.js
+++ jest.config.js
@@ -6,8 +6,24 @@ module.exports = {
 	  "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/tools/assetsTransformer.js",
 	  "\\.(css|less)$": "<rootDir>/tools/assetsTransformer.js"
 	},
-	moduleFileExtensions: ["js"],
-	testMatch: [ "**/__tests__/**/*.js", "**/?(*.)+(test).js" ],
+  moduleFileExtensions: [
+    "js", "jsx", "json", "ts", "tsx"
+  ],
+
+  testMatch: [
+    "**/__tests__/**/*.(js|jsx|ts|tsx)",
+    "**/?(*.)+(test).(js|jsx|ts|tsx)"
+  ],
+  transform:{
+    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.jsx?$": "babel-jest"
+  },
+  globals: {
+    "ts-jest": {
+      useBabelrc: true,
+      tsConfigFile: "tsconfig.json"
+    },
+  },
   verbose: true,
   notify: false,
   collectCoverage: false,
