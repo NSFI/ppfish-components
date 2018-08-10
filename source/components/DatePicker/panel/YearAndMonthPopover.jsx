6470c38cbc92229e5368191db95aea3de1d29c94
qingze <hzqingze@corp.netease.com>
2018-08-10 11:27:35 +0800

    datapicker update

--- source/components/DatePicker/panel/YearAndMonthPopover.jsx
+++ source/components/DatePicker/panel/YearAndMonthPopover.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import Popover from '../../Popover';
+import Popover from '../../Popover/index.tsx';
 import scrollIntoView from 'dom-scroll-into-view';
 
-import '../styles/YearAndMonthPopover.less'
+import '../styles/YearAndMonthPopover.less';
 
 export default class YearAndMonthPopover extends React.Component {
 
@@ -13,21 +13,24 @@ export default class YearAndMonthPopover extends React.Component {
     onChange: PropTypes.func,
     children: PropTypes.node,
     value: PropTypes.number
-  }
+  };
 
   constructor(props) {
     super(props);
 
     this.state = {
       visible: false,
-    }
+    };
   }
 
   scrollToOption = () => {
     const menu = this.refs.root;
     const active = menu.getElementsByClassName('active')[0];
-    active && scrollIntoView(active, menu);
-  }
+    active && scrollIntoView(active, menu, {
+      offsetTop: 91,
+      alignWithTop: true,
+    });
+  };
 
   handleOnClick(item) {
     this.setState({
@@ -38,15 +41,15 @@ export default class YearAndMonthPopover extends React.Component {
   }
 
   handleVisibleChange = (visible) => {
-    this.setState({ visible }, () => {
-      if(visible) {
-        this.scrollToOption()
+    this.setState({visible}, () => {
+      if (visible) {
+        this.scrollToOption();
       }
     });
-  }
+  };
 
   render() {
-    const { children, sourceData, value } = this.props;
+    const {children, sourceData, value} = this.props;
 
     const content = () => {
       return (
@@ -56,25 +59,26 @@ export default class YearAndMonthPopover extends React.Component {
         >
           {
             sourceData.map((item) => {
-            return (
-              <li
-                className={classNames({
-                  'fishd-year-and-month-popover-item': true,
-                  'active': value == item || typeof item === 'string' && item.slice(-1) == '月' && value == item.slice(0,-1)
-                })}
-                key={item}
-                onClick={this.handleOnClick.bind(this, item)}
-              >
-                {item}
-              </li>
-            );
-          })}
+              return (
+                <li
+                  className={classNames({
+                    'fishd-year-and-month-popover-item': true,
+                    'active': value == item || typeof item === 'string' && item.slice(-1) == '月' && value == item.slice(0,-1)
+                  })}
+                  key={item}
+                  onClick={this.handleOnClick.bind(this, item)}
+                >
+                  {item}
+                </li>
+              );
+            })}
         </div>
-      )
+      );
     };
 
     return (
       <Popover
+        transitionName={''}
         content={content()}
         trigger="click"
         placement="bottom"
