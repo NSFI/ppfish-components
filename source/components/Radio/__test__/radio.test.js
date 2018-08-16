da621000338caacb0c33c7cea2bc0644089763b9
wangxiaoling <wangxiaoling@corp.netease.com>
2018-08-16 16:53:19 +0800

    为Radio增加测试

---
+++ source/components/Radio/__test__/radio.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { shallow, render } from 'enzyme';
+import Radio from '../radio.tsx';
+import focusTest from '../../../tests/focusTest';
+
+describe('Radio', () => {
+  focusTest(Radio);
+
+  it('should render correctly', () => {
+    const wrapper = render(<Radio className="customized">Test</Radio>);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('responses hover events', () => {
+    const onMouseEnter = jest.fn();
+    const onMouseLeave = jest.fn();
+
+    const wrapper = shallow(
+      <Radio
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+      />
+    );
+
+    wrapper.simulate('mouseenter');
+    expect(onMouseEnter).toHaveBeenCalled();
+
+    wrapper.simulate('mouseleave');
+    expect(onMouseLeave).toHaveBeenCalled();
+  });
+});
