77a179f93f4dbf8b6b8f75752f62b564c19ca161
qingze <136786817@qq.com>
2018-08-22 15:31:27 +0800

    fix eslint

--- source/utils/bom.js
+++ source/utils/bom.js
@@ -4,7 +4,7 @@ export const getInternetExplorerVersion = () => {
   let rv = -1; // Return value assumes failure.
   if (navigator.appName == 'Microsoft Internet Explorer') {
     let ua = navigator.userAgent;
-    let re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+    let re  = new RegExp("MSIE ([0-9]{1,}[.0-9]{0,})");
     if (re.exec(ua) != null)
       rv = parseFloat( RegExp.$1 );
   }
