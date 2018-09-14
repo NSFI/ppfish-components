58663721f083decdea3cf72cad885ee6442f4da7
qingze <136786817@qq.com>
2018-09-14 18:01:46 +0800

    demo.test.js 引用同步更改

--- source/components/Cascader/__tests__/demo.test.js
+++ source/components/Cascader/__tests__/demo.test.js
@@ -1,10 +1,9 @@
-import { getPlainComponentList } from "../../../../site/utils/index.js";
-const plainComponentList = getPlainComponentList();
+import {plainComponents} from '../../../../site/componentsPage';
 
 let demo_component = 'cascader';
 
 let demoName = demo_component.substr(0, 1).toLowerCase() + demo_component.substr(1);
-const menuItem = plainComponentList.find(item => item.key === demoName);
+const menuItem = plainComponents.find(item => item.key === demoName);
 
 if(menuItem){
   const demoTest = require('../../../tests/demoTest').default;
