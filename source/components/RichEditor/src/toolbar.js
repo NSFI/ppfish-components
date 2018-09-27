9f522b9fba8936c39bed54248818954b75b7da7f
zhiyou <gtopia@163.com>
2018-09-27 17:09:16 +0800

    调整color模块默认值

--- source/components/RichEditor/src/toolbar.js
+++ source/components/RichEditor/src/toolbar.js
@@ -7,24 +7,21 @@ import Tooltip from '../../Tooltip/index.tsx';
 import Popover from '../../Popover/index.tsx';
 
 let defaultColors = [
-  'rgb(  0,   0,   0)', 'rgb(230,   0,   0)', 'rgb(255, 153,   0)',
-  'rgb(255, 255,   0)', 'rgb(  0, 138,   0)', 'rgb(  0, 102, 204)',
-  'rgb(153,  51, 255)', 'rgb(255, 255, 255)', 'rgb(250, 204, 204)',
-  'rgb(255, 235, 204)', 'rgb(255, 255, 204)', 'rgb(204, 232, 204)',
-  'rgb(204, 224, 245)', 'rgb(235, 214, 255)', 'rgb(187, 187, 187)',
-  'rgb(240, 102, 102)', 'rgb(255, 194, 102)', 'rgb(255, 255, 102)',
-  'rgb(102, 185, 102)', 'rgb(102, 163, 224)', 'rgb(194, 133, 255)',
-  'rgb(136, 136, 136)', 'rgb(161,   0,   0)', 'rgb(178, 107,   0)',
-  'rgb(178, 178,   0)', 'rgb(  0,  97,   0)', 'rgb(  0,  71, 178)',
-  'rgb(107,  36, 178)', 'rgb( 68,  68,  68)', 'rgb( 92,   0,   0)',
-  'rgb(102,  61,   0)', 'rgb(102, 102,   0)', 'rgb(  0,  55,   0)',
-  'rgb(  0,  41, 102)', 'rgb( 61,  20,  10)',
+  '#E53333', '#E56600', '#FF9900',
+  '#64451D', '#DFC5A4', '#FFE500',
+  '#009900', '#006600', '#99BB00',
+  '#B8D100', '#60D978', '#00D5FF',
+  '#337FE5', '#003399', '#4C33E5',
+  '#9933E5', '#CC33E5', '#EE33EE',
+  '#ffffff', '#cccccc', '#999999',
+  '#666666', '#333333', '#000000',
 ].map(function(color, index) {
   return (
     <button
       className="color-item"
       key={"default_color_" + index}
       value={color}
+      title={color}
       style={{backgroundColor: color}}
     />
   );
@@ -191,6 +188,7 @@ class CustomToolbar extends PureComponent {
                   className="color-item"
                   key={"custom_color_" + index}
                   value={color}
+                  title={color}
                   style={{backgroundColor: color}}
                 />
               );
