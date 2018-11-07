018816cb6b878e31bf8e7e63677cb6ce2080a553
qingze <136786817@qq.com>
2018-11-07 17:02:19 +0800

    清理冗余文件夹、冗余代码

--- site/index.js
+++ site/index.js
@@ -5,14 +5,12 @@ import {Router, Route, hashHistory, Redirect, IndexRoute} from 'react-router';
 import './styles/index.less';
 import Components from './pages/components';
 import Home from './pages/home';
-import Spec from './pages/spec';
 import Demo from './pages/demo';
 
 const App = (Components) => (
   <AppContainer>
     <Router history={hashHistory}>
       <Route path="/home" component={Home}/>
-      <Route path="/spec" component={Spec}/>
       <Route path="/components" component={Components}>
         <IndexRoute component={Demo}/>
         <Route path=":demo" component={Demo}/>
--- source/constants/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * 打包文件已将constants整体打包到vendor.js，为了合理利用浏览器缓存，
- * 其他页面引用constants请使用import from '../constants'，
- * 引用整个文件夹就行了，不需要指定引用到'../constants/limit'
- */
-import { limit } from './limit';
-const json = Object.assign({}, limit);
-export default json;
