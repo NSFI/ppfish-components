ea0cd063deff96fcdd8cc39b5a9c0ea9c7c960d5
qingze <136786817@qq.com>
2018-11-20 16:34:57 +0800

    去掉card依赖

--- site/pages/demo/source.js
+++ site/pages/demo/source.js
@@ -3,5 +3,3 @@ export {Resizable} from 'react-resizable';
 export copy from 'copy-to-clipboard';
 // ppfish发布的组件
 export * from '../../../source/components';
-// demo 中使用的card
-export Card from '../../../source/components/Card/index.tsx';
