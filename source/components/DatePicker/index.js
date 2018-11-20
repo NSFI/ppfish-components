012b36ad246ba125af6be76bcdf985255722fe26
wangxiaoling <wangxiaoling@corp.netease.com>
2018-11-20 16:25:51 +0800

    TimePicker的导出方式改为default导出

--- source/components/DatePicker/index.js
+++ source/components/DatePicker/index.js
@@ -1,10 +1,7 @@
-import TimeSelect from './TimeSelect.jsx';
-import TimePicker from './TimePicker.jsx';
 import DatePicker from './DatePicker.jsx';
 import DateRangePicker from './DateRangePicker.jsx';
 import './style/index.less';
 
-TimePicker.TimeSelect = TimeSelect;
 DatePicker.DateRangePicker = DateRangePicker;
 
-export { TimePicker, DatePicker };
+export default DatePicker;
---
+++ source/components/TimePicker/index.js
@@ -0,0 +1,7 @@
+import TimeSelect from '../DatePicker/TimeSelect.jsx';
+import TimePicker from '../DatePicker/TimePicker.jsx';
+import '../DatePicker/style/index.less';
+
+TimePicker.TimeSelect = TimeSelect;
+
+export default TimePicker;
