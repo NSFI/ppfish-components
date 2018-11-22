242f3ac084ee7933b53772956103b5105c548769
qingze <136786817@qq.com>
2018-11-22 10:25:14 +0800

    增加emoji表情说明

--- site/componentsPage/changeLog/index.js
+++ site/componentsPage/changeLog/index.js
@@ -15,6 +15,16 @@ export default class ChangeLog extends React.Component {
     return (
       <div className="changelog">
         <h1 className="changelog-title">更新日志</h1>
+        <div className="changelog-description">
+          <h3>Emoji表情说明</h3>
+          <ul>
+            <li>🎊 新增特性，如新增组件、 API等</li>
+            <li>🔨 视觉调整、交互调整、功能优化等</li>
+            <li>🐛 bug修复</li>
+            <li>☠ 废弃特性，如废弃组件、 API等</li>
+            <li>🔥 变化较大，需要重点关注的更新</li>
+          </ul>
+        </div>
         <Timeline className="changelog-timeline">
           {
             changeLog.map((log, i) => (
