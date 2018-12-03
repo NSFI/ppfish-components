0d5eaa219770845cec4c71b489aa1d4bc52fa882
zhiyou <gtopia@163.com>
2018-12-03 17:29:02 +0800

    修复在超链接后输入回车光标被异常添加超链接的问题

--- source/components/RichEditor/src/formats/link.js
+++ source/components/RichEditor/src/formats/link.js
@@ -3,14 +3,22 @@ const Inline = Quill.import('blots/inline');
 
 class Link extends Inline {
   static create(value) {
+    this.formatCursor = false;
     const node = super.create(value);
     node.setAttribute('href', value);
     node.setAttribute('target', '_blank');
     return node;
   }
 
-  static formats(domNode) {
-    return domNode.getAttribute('href');
+  static formats(node) {
+    // 修复在超链接后输入回车光标被异常添加超链接的问题
+    let domChildren = node.children;
+    // let containsCursor = /<\s*span\s*class\s*=\s*['"]\s*ql-cursor\s*['"]\s*>\s*\ufeff\s*<\s*\/\s*span\s*>/gi;
+    if (!this.formatCursor && domChildren && domChildren.length==1 && domChildren[0].innerText=="\ufeff") {
+      return;
+    };
+
+    return node.getAttribute('href');
   }
 
   // static sanitize(url) {
@@ -21,12 +29,15 @@ class Link extends Inline {
     if (name !== this.statics.blotName || !value) {
       super.format(name, value);
     } else {
+      // 在超链接内输入回车时需要为光标添加超链接
+      this.statics.formatCursor = true;
       this.domNode.setAttribute('href', value);
     }
   }
 }
 Link.blotName = 'myLink';
 Link.tagName = 'A';
+Link.formatCursor = false;  // 是否为光标添加超链接
 // Link.SANITIZED_URL = 'about:blank';
 // Link.PROTOCOL_WHITELIST = ['http', 'https', 'mailto', 'tel'];
 
