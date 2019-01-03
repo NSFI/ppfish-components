32ee9dcaeb165895c2a1b9b69a2704a78fadc699
qingze <136786817@qq.com>
2019-01-03 12:21:10 +0800

    Select 测试用例添加和修复

--- source/components/Select/__tests__/index.test.js
+++ source/components/Select/__tests__/index.test.js
@@ -5,6 +5,7 @@ import Select from '../index';
 describe('<Select />', () => {
   const props = {
     onVisibleChange: jest.fn(),
+    visible: false,
   };
   let wrapper;
   beforeAll(() => {
@@ -19,13 +20,13 @@ describe('<Select />', () => {
   });
 
   test('Select组件能够被正确渲染', () => {
-    wrapper = mount((
+    wrapper = mount(
       <Select {...props}>
         <Select.Option value={1}>1</Select.Option>
         <Select.Option value={2}>2</Select.Option>
         <Select.Option value={3}>3</Select.Option>
       </Select>
-    ));
+    );
     const inst = wrapper.instance();
     expect(inst).toBeInstanceOf(Select);
     expect(wrapper.find('.fishd-select').exists()).toBe(true);
@@ -33,14 +34,17 @@ describe('<Select />', () => {
 
   test('Select组件能够被正常打开', () => {
     wrapper.find('.fishd-select').simulate('click');
-    expect(wrapper.state('visible')).toBe(true);
-    expect(onVisibleChange).toHaveBeenCalledWith(true);
+    expect(wrapper.state('popupVisible')).toBe(true);
+    expect(props.onVisibleChange).toHaveBeenCalledWith(true);
   });
-
 
   it('Select组件能够visible受控', () => {
-    expect(wrapper.instance().getComponent().props.visible).toBe(true);
-    wrapper.setProps({ visible: false });
-    expect(wrapper.instance().getComponent().props.visible).toBe(false);
+    expect(wrapper.instance().props.visible).toBe(false);
+    wrapper.setProps({visible: true});
+    expect(wrapper.instance().props.visible).toBe(true);
   });
+
+  it('Select选项个数为3', () => {
+    expect(wrapper.find('.fishd-select-dropdown-option-item').length).toBe(3)
+  })
 });
