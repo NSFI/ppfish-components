eb33137dede8b6137d9ec0862778c8461389d7f5
zhiyou <gtopia@163.com>
2019-04-09 14:24:44 +0800

    调整官网样式

--- site/pages/home/index.js
+++ site/pages/home/index.js
@@ -19,7 +19,7 @@ export default class Home extends React.Component {
             </div>
             <div className="right-desc">
               <h1 className="title">Fish Design</h1>
-              <p className="desc">基于 React 实现的高质量的企业级 UI 组件库，帮助设计者与开发者快速构建系统。</p>
+              <p className="desc">基于 React 实现的高质量的企业级 UI 组件库，<br/>帮助设计者与开发者快速构建系统。</p>
               <div className="button-group">
                 <Link to="/components/" className="button start">Start</Link>
                 <a href="http://github.com/NSFI/ppfish-components/" target="_blank" className="button github">&nbsp;</a>
