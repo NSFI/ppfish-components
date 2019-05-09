19deebbb7e28ac1d6bcd0c72b982904c811204b5
zhiyou <gtopia@163.com>
2019-05-09 16:50:56 +0800

    添加组件维护人，调整维护人列表样式

--- site/pages/demo/renderer.js
+++ site/pages/demo/renderer.js
@@ -16,10 +16,21 @@ renderer.heading = function (text, level, raw) {
     let id = text;
     // h1
     if (level === 1) {
-      const textId = text.replace(/【(.+)】/, '');
+      const textId = text.replace(/\s*【(.+)】\s*/, '');
       id = textId === text ? text : textId;
-      text = text.replace(/【(.+)】/, function (ownerList) {
-        return `<div class="md-owner"><div class="owner-title"><i class="fishdicon fishdicon-user-line"></i></div><ul class="owner-list">${ownerList.slice(1, -1).split('|').map(owner => `<li class="item">${owner}</li>`).join('')}</ul></div>`;
+      text = text.replace(/\s*【(.+)】\s*/, function (ownerList) {
+        return (
+          `<div class="md-owner">
+            <div class="owner-title">
+              <i class="fishdicon fishdicon-user-line"></i>
+            </div>
+            <ul class="owner-list">
+              ${ownerList.trim().slice(1, -1).split('|').map(owner => {
+                return `<li class="item">${owner.trim()}</li>`;
+              }).join('')}
+            </ul>
+          </div>`
+        );
       });
     }
     return '<h' + level + ' id="' + id + '" class="md-heading">' + text + '</h' + level + '>\n';
