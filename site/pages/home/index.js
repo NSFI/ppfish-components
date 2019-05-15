a8a6753fef6635a0bd7db28598ddca36893609c2
zhiyou <gtopia@163.com>
2019-05-15 15:34:47 +0800

    为官网的设计原则部分添加动效

--- site/pages/home/index.js
+++ site/pages/home/index.js
@@ -1,12 +1,55 @@
 import React from 'react';
 import {Link} from 'react-router';
-
+import lottie from 'lottie-web';
+import conciseData from '../../assets/lottie/concise.json';
+import immediateData from '../../assets/lottie/immediate.json';
+import elegentData from '../../assets/lottie/elegent.json';
+import adaptabilityData from '../../assets/lottie/adaptability.json';
 import Layout from '../common/layout';
 import './index.less';
 
 export default class Home extends React.Component {
   constructor(props) {
     super(props);
+  }
+
+  componentDidMount() {
+    let lottieItems = [{
+      id: 'lottie_concise',
+      data: conciseData
+    }, {
+      id: 'lottie_immediate',
+      data: immediateData
+    }, {
+      id: 'lottie_elegent',
+      data: elegentData
+    }, {
+      id: 'lottie_adaptability',
+      data: adaptabilityData
+    }];
+
+    lottieItems.forEach((item) => {
+      let el = document.getElementById(item.id);
+      if (!el) return;
+
+      let animation = lottie.loadAnimation({
+        container: el,
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        animationData: item.data
+      });
+
+      el.addEventListener('mouseenter', function() {
+        animation.setDirection(1);
+        animation.play();
+      });
+
+      el.addEventListener('mouseleave', function() {
+        animation.setDirection(-1);
+        animation.play();
+      });
+    });
   }
 
   render() {
@@ -30,25 +73,25 @@ export default class Home extends React.Component {
             <h2 className="title">设计原则</h2>
             <div className="design-list">
               <div className="principle">
-                <img src={'//ysf.nosdn.127.net/wswhgoggxhbdrtkxefigwmwgefgicdcd'} alt="简洁｜Concise"/>
+                <div id="lottie_concise" className="lottie-item" />
                 <h3>简洁｜Concise</h3>
                 <i className="split-icon"/>
                 <p>如无必要 勿增实体：慎重筛选客户当前需要信息内容</p>
               </div>
               <div className="principle">
-                <img src={'//ysf.nosdn.127.net/ryatctvlcqyowiegkfbgcaiiqrxcnfha'} alt="直接｜Immediate"/>
+                <div id="lottie_immediate" className="lottie-item" />
                 <h3>直接｜Immediate</h3>
                 <i className="split-icon"/>
                 <p>提供用户操作后的直接反馈，保证用户的操作结果符合预期</p>
               </div>
               <div className="principle">
-                <img src={'//ysf.nosdn.127.net/xvbiyraokrdcmhrtoxddexqcognzsube'} alt="优雅｜Elegent"/>
+                <div id="lottie_elegent" className="lottie-item" />
                 <h3>优雅｜Elegent</h3>
                 <i className="split-icon"/>
                 <p>设计方案追求优雅，給使用者有质感的操作感受</p>
               </div>
               <div className="principle">
-                <img src={'//ysf.nosdn.127.net/xvrqcrjowyhsnnpqdanllorwtsjlfqhu'} alt="适应性｜Adaptability"/>
+                <div id="lottie_adaptability" className="lottie-item" />
                 <h3>适应性｜Adaptability</h3>
                 <i className="split-icon"/>
                 <p>设计方案提供可扩展能力及适应性，适应不同模式的企业使用</p>
