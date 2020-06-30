eeef95bf3b048664d357db54bb2f8060205f389d
Alan Gao <gtopia@163.com>
2020-06-30 17:02:16 +0800

    插入图片时，后面不再自动添加一个空格，避免微信端富文本消息异常

--- source/components/RichEditor/src/modules/imageDrop.js
+++ source/components/RichEditor/src/modules/imageDrop.js
@@ -50,8 +50,7 @@ export default class ImageDrop {
     if (!attrs.src) return;
 
     this.quill.insertEmbed(index, 'myImage', attrs);
-    this.quill.insertText(index + 1, ' ');
-    this.quill.setSelection(index + 2);
+    this.quill.setSelection(index + 1);
   }
 
   // Extract image URIs a list of files from evt.dataTransfer or evt.clipboardData
