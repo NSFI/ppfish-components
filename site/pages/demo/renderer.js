12742e2857a87d6e08aabec1b0ac93e67fc04391
Alan Gao <gtopia@163.com>
2020-11-11 11:30:46 +0800

    隐藏组件维护人员信息

--- site/pages/demo/renderer.js
+++ site/pages/demo/renderer.js
@@ -12,31 +12,32 @@ renderer.paragraph = function (text) {
   return '<p class="md-paragraph">' + text + '</p>\n';
 };
 renderer.heading = function (text, level, raw) {
-  if (this.options.headerIds) {
-    let id = text;
-    // h1
-    if (level === 1) {
-      const textId = text.replace(/\s*【(.+)】\s*/, '');
-      id = textId === text ? text : textId;
-      text = text.replace(/\s*【(.+)】\s*/, function (ownerList) {
-        return (
-          `<div class="md-owner">
-            <div class="owner-title">
-              <i class="fishdicon fishdicon-user-line"></i>
-            </div>
-            <ul class="owner-list">
-              ${ownerList.trim().slice(1, -1).split('|').map(owner => {
-                return `<li class="item">${owner.trim()}</li>`;
-              }).join('')}
-            </ul>
-          </div>`
-        );
-      });
-    }
-    return '<h' + level + ' id="' + id + '" class="md-heading">' + text + '</h' + level + '>\n';
-  }
+  // if (this.options.headerIds) {
+  //   let id = text;
+  //   // h1
+  //   if (level === 1) {
+  //     const textId = text.replace(/\s*【(.+)】\s*/, '');
+  //     id = textId === text ? text : textId;
+  //     text = text.replace(/\s*【(.+)】\s*/, function (ownerList) {
+  //       return (
+  //         `<div class="md-owner">
+  //           <div class="owner-title">
+  //             <i class="fishdicon fishdicon-user-line"></i>
+  //           </div>
+  //           <ul class="owner-list">
+  //             ${ownerList.trim().slice(1, -1).split('|').map(owner => {
+  //               return `<li class="item">${owner.trim()}</li>`;
+  //             }).join('')}
+  //           </ul>
+  //         </div>`
+  //       );
+  //     });
+  //   }
+  //   return '<h' + level + ' id="' + id + '" class="md-heading">' + text + '</h' + level + '>\n';
+  // }
   // ignore IDs
-  return '<h' + level + ' class="md-heading" >' + text + '</h' + level + '>\n';
+  let title = text.replace(/\s*【(.+)】\s*/, '');
+  return '<h' + level + ' class="md-heading" >' + title + '</h' + level + '>\n';
 };
 
 export default renderer;
