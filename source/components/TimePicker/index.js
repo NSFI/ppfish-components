60b997cfefa2fb2486140b336a0a7198bb182506
bushuai <ibushuai@gmail.com>
2020-12-17 20:07:16 +0800

    refactor(ts): TimePicker

--- source/components/TimePicker/index.js
+++
@@ -1,6 +0,0 @@
-import TimeSelect from '../DatePicker/TimeSelect.js';
-import TimePicker from '../DatePicker/TimePicker.js';
-
-TimePicker.TimeSelect = TimeSelect;
-
-export default TimePicker;
---
+++ source/components/TimePicker/index.tsx
@@ -0,0 +1,6 @@
+import TimeSelect from '../DatePicker/TimeSelect';
+import TimePicker from '../DatePicker/TimePicker';
+
+TimePicker.TimeSelect = TimeSelect;
+
+export default TimePicker;
