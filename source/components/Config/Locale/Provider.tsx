dacb1af951f55a68b4d6fdb6dde839098d91c042
SummerOverture <740834798@qq.com>
2020-12-31 14:14:37 +0800

    feat: runtimeLocale增加默认Locale

--- source/components/Config/Locale/Provider.tsx
+++ source/components/Config/Locale/Provider.tsx
@@ -3,14 +3,20 @@ import React from 'react'
 import { Provider } from './Context';
 import { getLocale } from './Consumer';
 
-let runtimeLocale = {};
+let runtimeLocale: {
+  locale?: string,
+} = {};
 
 export function getRuntimeLocale(componentName) {
+  if (!runtimeLocale.locale) {
+    // 获取默认Locale;
+    changeRuntimeLocale();
+  }
   return runtimeLocale[componentName] || {};
 }
 
 // 部分组件如Modal 可以通过函数式调用  所以需要runtimeLocale提供支撑
-export function changeRuntimeLocale(LocaleValue = 'en_US') {
+export function changeRuntimeLocale(LocaleValue = 'zh_CN') {
   runtimeLocale = getLocale(LocaleValue);
 };
 
