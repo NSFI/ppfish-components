2dd0d23b2f16be8788d1ba00d999e18ba404b446
邱瑶瑶 <qiuyaoyao@corp.netease.com>
2020-12-31 12:24:01 +0800

    feat:table spin select locale support

--- source/components/Locale/index.ts
+++ source/components/Locale/index.ts
@@ -5,19 +5,23 @@ import en_US from './en_US';
 type keyStringObj = {
   [propName: string]: string | object,
 }
-
+type keyString = {
+  [propName: string]: string,
+}
 
 export interface LocaleProperties {
-  locale: string,
-  AudioPlayer?: keyStringObj,
-  AutoComplete?: keyStringObj,
-  Cascader?: keyStringObj,
-  global?: keyStringObj,
-  Table?: keyStringObj,
-  Modal?: keyStringObj,
+  locale: string;
+  AudioPlayer?: keyStringObj;
+  AutoComplete?: keyStringObj;
+  Cascader?: keyStringObj;
+  global?: keyStringObj;
+  Table?: keyString;
+  Modal?: keyStringObj;
   Form?: keyStringObj & {
-    defaultValidateMessages: keyStringObj,
-  }
+    defaultValidateMessages: keyStringObj;
+  };
+  Spin?: keyStringObj;
+  Select?: keyString;
 };
 
 const Locale = {
--- source/components/Locale/zh_CN.ts
+++ source/components/Locale/zh_CN.ts
@@ -3,23 +3,24 @@ import { LocaleProperties } from '.';
 const localeValues: LocaleProperties = {
   locale: 'zh_CN',
   global: {
-    placeholder: '请选择',
+    placeholder: '请选择'
   },
   AudioPlayer: {
-    notSupport: '您的浏览器不支持 audio 标签。',
+    notSupport: '您的浏览器不支持 audio 标签。'
   },
   AutoComplete: {
-    notFoundContent: '无匹配结果',
+    notFoundContent: '无匹配结果'
   },
   Cascader: {
     placeholder: '请选择',
-    notFoundContent: '无匹配结果',
+    notFoundContent: '无匹配结果'
   },
   Table: {
     filterTitle: '筛选',
     filterConfirm: '确定',
     filterReset: '重置',
     filterEmptyText: '无筛选项',
+    emptyText: '暂无数据',
     selectAll: '全选当页',
     selectInvert: '反选当页',
     selectionAll: '全选所有',
@@ -29,11 +30,15 @@ const localeValues: LocaleProperties = {
     triggerDesc: '点击降序',
     triggerAsc: '点击升序',
     cancelSort: '取消排序',
+    modalTitle: '选择需要展示的数据项',
+    defaultOptionTitle: '默认展示数据项',
+    okText: '确定',
+    cancelText: '取消',
   },
   Modal: {
     okText: '确定',
     cancelText: '取消',
-    justOkText: '知道了',
+    justOkText: '知道了'
   },
   Form: {
     optional: '（可选）',
@@ -41,9 +46,23 @@ const localeValues: LocaleProperties = {
       default: '字段验证错误${label}',
       required: '请输入${label}',
       enum: '${label}必须是其中一个[${enum}]',
-      whitespace: '${label}不能为空字符',
-    },
+      whitespace: '${label}不能为空字符'
+    }
   },
+  Spin: {
+    loading: '加载中'
+  },
+  Select: {
+    errorMessage: '超过选项上限',
+    multipleSelectAllText: '所有选项',
+    notFoundContent: '无匹配结果',
+    placeholder: '请选择',
+    searchPlaceholder: '请输入关键字',
+    selectAllText: '选择所有',
+    confirmText: '确定',
+    cancelText: '取消',
+    loading: '加载中...'
+  }
 };
 
 export default localeValues;
