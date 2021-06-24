88465740c7db7ec7aec253f69c07e62c79b26160
SummerOverture <740834798@qq.com>
2021-06-24 11:42:13 +0800

    refactor: alert组件hook重构完成

--- source/components/Alert/Alert.tsx
+++ source/components/Alert/Alert.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
 import Animate from 'rc-animate';
 import Icon from '../Icon';
 import classNames from 'classnames';
@@ -44,118 +43,113 @@ export interface AlertProps {
   banner?: boolean;
 }
 
-export default class Alert extends React.Component<AlertProps, any> {
-  constructor(props: AlertProps) {
-    super(props);
-    this.state = {
-      closing: true,
-      closed: false
-    };
-  }
-  handleClose = (e: React.MouseEvent<HTMLAnchorElement>) => {
+const Alert: React.FC<AlertProps> = props => {
+  const { onClose,afterClose } = props;
+  const ref = React.useRef<React.ReactNode>();
+  const [closing, setClosing] = React.useState<boolean>(true);
+  const [closed, setClosed] = React.useState<boolean>(false);
+
+  const handleClose = (e: React.MouseEvent<HTMLAnchorElement>) => {
     e.preventDefault();
-    let dom = ReactDOM.findDOMNode(this) as HTMLElement;
+    let dom = ref.current as HTMLElement;
     dom.style.height = `${dom.offsetHeight}px`;
     // Magic code
     // 重复一次后才能正确设置 height
     dom.style.height = `${dom.offsetHeight}px`;
 
-    this.setState({
-      closing: false
-    });
-    (this.props.onClose || noop)(e);
+    setClosing(false);
+    (onClose || noop)(e);
   };
-  animationEnd = () => {
-    this.setState({
-      closed: true,
-      closing: true
-    });
-    (this.props.afterClose || noop)();
+  const animationEnd = () => {
+    setClosed(true);
+    setClosing(true);
+    (afterClose || noop)();
   };
-  render() {
-    let {
-      closable,
-      description,
-      type,
-      prefixCls = 'fishd-alert',
-      message,
-      closeText,
-      showIcon,
-      banner,
-      className = '',
-      style,
-      iconType
-    } = this.props;
 
-    // banner模式默认有 Icon
-    showIcon = banner && showIcon === undefined ? true : showIcon;
-    // banner模式默认为警告
-    type = banner && type === undefined ? 'warning' : type || 'info';
+  let {
+    closable,
+    description,
+    type,
+    prefixCls = 'fishd-alert',
+    message,
+    closeText,
+    showIcon,
+    banner,
+    className = '',
+    style,
+    iconType
+  } = props;
 
-    if (!iconType) {
-      switch (type) {
-        case 'success':
-          iconType = 'hints-success';
-          break;
-        case 'info':
-          iconType = 'hints-notification';
-          break;
-        case 'error':
-          iconType = 'hints-error';
-          break;
-        case 'warning':
-          iconType = 'hints-warning';
-          break;
-        // 展示空icon
-        default:
-          iconType = 'default';
-      }
+  // banner模式默认有 Icon
+  showIcon = banner && showIcon === undefined ? true : showIcon;
+  // banner模式默认为警告
+  type = banner && type === undefined ? 'warning' : type || 'info';
 
-      // use outline icon in alert with description
-      if (description) {
-        iconType += '-o';
-      }
+  if (!iconType) {
+    switch (type) {
+      case 'success':
+        iconType = 'hints-success';
+        break;
+      case 'info':
+        iconType = 'hints-notification';
+        break;
+      case 'error':
+        iconType = 'hints-error';
+        break;
+      case 'warning':
+        iconType = 'hints-warning';
+        break;
+      // 展示空icon
+      default:
+        iconType = 'default';
     }
 
-    let alertCls = classNames(
-      prefixCls,
-      {
-        [`${prefixCls}-${type}`]: true,
-        [`${prefixCls}-close`]: !this.state.closing,
-        [`${prefixCls}-with-description`]: !!description,
-        [`${prefixCls}-no-icon`]: !showIcon,
-        [`${prefixCls}-banner`]: !!banner
-      },
-      className
-    );
-
-    // closeable when closeText is assigned
-    if (closeText) {
-      closable = true;
+    // use outline icon in alert with description
+    if (description) {
+      iconType += '-o';
     }
-
-    const closeIcon = closable ? (
-      <a onClick={this.handleClose} className={`${prefixCls}-close-icon`}>
-        {closeText || <Icon type="close-modal-line" />}
-      </a>
-    ) : null;
-
-    const dataOrAriaProps = getDataOrAriaProps(this.props);
-
-    return this.state.closed ? null : (
-      <Animate
-        component=""
-        showProp="data-show"
-        transitionName={`${prefixCls}-slide-up`}
-        onEnd={this.animationEnd}
-      >
-        <div data-show={this.state.closing} className={alertCls} style={style} {...dataOrAriaProps}>
-          {showIcon ? <Icon className={`${prefixCls}-icon`} type={iconType} /> : null}
-          <span className={`${prefixCls}-message`}>{message}</span>
-          <span className={`${prefixCls}-description`}>{description}</span>
-          {closeIcon}
-        </div>
-      </Animate>
-    );
   }
-}
+
+  let alertCls = classNames(
+    prefixCls,
+    {
+      [`${prefixCls}-${type}`]: true,
+      [`${prefixCls}-close`]: !closing,
+      [`${prefixCls}-with-description`]: !!description,
+      [`${prefixCls}-no-icon`]: !showIcon,
+      [`${prefixCls}-banner`]: !!banner
+    },
+    className
+  );
+
+  // closeable when closeText is assigned
+  if (closeText) {
+    closable = true;
+  }
+
+  const closeIcon = closable ? (
+    <a onClick={handleClose} className={`${prefixCls}-close-icon`}>
+      {closeText || <Icon type="close-modal-line" />}
+    </a>
+  ) : null;
+
+  const dataOrAriaProps = getDataOrAriaProps(props);
+
+  return closed ? null : (
+    <Animate
+      component=""
+      showProp="data-show"
+      transitionName={`${prefixCls}-slide-up`}
+      onEnd={animationEnd}
+    >
+      <div ref={ref} data-show={closing} className={alertCls} style={style} {...dataOrAriaProps}>
+        {showIcon ? <Icon className={`${prefixCls}-icon`} type={iconType} /> : null}
+        <span className={`${prefixCls}-message`}>{message}</span>
+        <span className={`${prefixCls}-description`}>{description}</span>
+        {closeIcon}
+      </div>
+    </Animate>
+  );
+};
+
+export default Alert;
