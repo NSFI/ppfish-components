98921ad953cfb926b37c3bb48a261d3f3a743100
chenchen32 <qianchengcn@foxmail.com>
2021-09-10 10:44:14 +0800

    refactor：FooterToolbar重构完成，新增单测

---
+++ source/components/FooterToolbar/__tests__/index.test.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import FooterToolbar from '../FooterToolbar';
+
+describe('FooterToolbar', () => {
+  let originGetComputedStyle;
+  beforeAll(() => {
+    originGetComputedStyle = Object.getOwnPropertyDescriptor(window, 'getComputedStyle').value;
+    Object.defineProperty(window, 'getComputedStyle', {
+      value: () => ({
+        height: 100
+      })
+    });
+  })
+
+  afterAll(() => {
+    Object.defineProperty(window, 'getComputedStyle', {
+      value: originGetComputedStyle
+    });
+  })
+
+  it('FooterToolbar should render', () => {
+    const Page = () => {
+      return (
+        <div style={{ position: 'relative'}}>
+          <FooterToolbar>footer</FooterToolbar>
+        </div>
+      );
+    };
+    const wrapper = mount(<Page/>);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
