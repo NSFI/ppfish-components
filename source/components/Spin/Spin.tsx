d9abf0c545d569cda6ed73166de556b6ca25de25
storm <847073376@qq.com>
2021-09-30 11:35:12 +0800

    refactor: Spin组件重构完成，更新单元测试

--- source/components/Spin/Spin.tsx
+++ source/components/Spin/Spin.tsx
@@ -55,153 +55,101 @@ function renderIndicator(props: SpinProps): React.ReactNode {
   );
 }
 
-class Spin extends React.Component<SpinProps, SpinState> {
-  static Container: typeof Container;
-  static TextLoading: typeof TextLoading;
-
-  static defaultProps = {
-    prefixCls: 'fishd-spin',
-    spinning: true,
-    size: 'default' as SpinSize,
-    wrapperClassName: ''
-  };
-
-  static propTypes = {
-    prefixCls: PropTypes.string,
-    className: PropTypes.string,
-    spinning: PropTypes.bool,
-    size: PropTypes.oneOf(['small', 'default', 'large']),
-    wrapperClassName: PropTypes.string,
-    indicator: PropTypes.node
-  };
-
-  static setDefaultIndicator(indicator: React.ReactNode) {
-    defaultIndicator = indicator;
-  }
-
-  debounceTimeout: number;
-  delayTimeout: number;
-
-  constructor(props: SpinProps) {
-    super(props);
-    const spinning = props.spinning;
-    this.state = {
-      spinning
-    };
-  }
-
-  static getDerivedStateFromProps(nextProps: SpinProps, prevState: SpinState) {
-    let { spinning, delay } = nextProps;
-    if (prevState.spinning !== spinning) {
-      if (spinning == false || isNaN(Number(delay)) || delay === 0) {
-        // spinning -> false
-        // spinning -> true && delay -> falsy
-        return {
-          ...prevState,
-          spinning
-        };
-      }
-    }
-    return null;
-  }
-
-  isNestedPattern() {
-    return !!(this.props && this.props.children);
-  }
-
-  componentDidMount() {
-    const { spinning, delay } = this.props;
-    if (spinning && delay && !isNaN(Number(delay))) {
-      this.setState({ spinning: false });
-      this.delayTimeout = window.setTimeout(() => this.setState({ spinning }), delay);
-    }
-  }
-
-  componentWillUnmount() {
-    if (this.debounceTimeout) {
-      clearTimeout(this.debounceTimeout);
-    }
-    if (this.delayTimeout) {
-      clearTimeout(this.delayTimeout);
-    }
-  }
-
-  componentDidUpdate(prevProps: SpinProps) {
-    const spinning = this.props.spinning;
-    const prevSpinning = prevProps.spinning;
-    const { delay } = this.props;
-
-    if (this.debounceTimeout) {
-      clearTimeout(this.debounceTimeout);
-    }
-    if (prevSpinning && !spinning) {
-      this.debounceTimeout = window.setTimeout(() => this.setState({ spinning }), 200);
-      if (this.delayTimeout) {
-        clearTimeout(this.delayTimeout);
-      }
-    } else {
-      if (spinning && delay && !isNaN(Number(delay))) {
-        if (this.delayTimeout) {
-          clearTimeout(this.delayTimeout);
-        }
-        this.delayTimeout = window.setTimeout(() => this.setState({ spinning }), delay);
-      } else if (spinning !== this.state.spinning) {
-        this.setState({ spinning });
-      }
-    }
-  }
-
-  render() {
-    const { className, size, prefixCls, tip, wrapperClassName, ...restProps } = this.props;
-    const { spinning } = this.state;
-
-    const spinClassName = classNames(
-      prefixCls,
-      {
-        [`${prefixCls}-sm`]: size === 'small',
-        [`${prefixCls}-lg`]: size === 'large',
-        [`${prefixCls}-spinning`]: spinning,
-        [`${prefixCls}-show-text`]: !!tip
-      },
-      className
-    );
-
-    // fix https://fb.me/react-unknown-prop
-    const divProps = omit(restProps, ['spinning', 'delay', 'indicator']);
-
-    const spinElement = (
-      <div {...divProps} className={spinClassName}>
-        {renderIndicator(this.props)}
-        {tip ? <span className={`${prefixCls}-text`}>{tip}</span> : null}
-      </div>
-    );
-    if (this.isNestedPattern()) {
-      let animateClassName = prefixCls + '-nested-loading';
-      if (wrapperClassName) {
-        animateClassName += ' ' + wrapperClassName;
-      }
-      const nestedClassName = classNames({
-        [`${prefixCls}-nested`]: true,
-        [`${prefixCls}-blur`]: spinning
-      });
-      return (
-        <Animate
-          {...divProps}
-          component="div"
-          className={animateClassName}
-          style={null}
-          transitionName="fade"
-        >
-          {spinning && <div key="loading">{spinElement}</div>}
-          <div className={nestedClassName} key="nested">
-            {this.props.children}
-          </div>
-        </Animate>
-      );
-    }
-    return spinElement;
-  }
+interface SpinInterface extends React.FC<SpinProps> {
+  Container: typeof Container,
+  TextLoading: typeof TextLoading,
+  setDefaultIndicator: (indicator: React.ReactNode) => void
 }
 
-polyfill(Spin);
+const Spin: SpinInterface = (props) => {
+  const { className, size, prefixCls, tip, wrapperClassName, spinning, delay, indicator, ...restProps } = props;
+  const [innerSpinning, setInnerSpinning] = React.useState(props.spinning);
+
+  const delayTimeout = React.useRef<number>();
+  React.useEffect(() => {
+
+    if (spinning && delay && !isNaN(Number(delay))) {
+      clearDelayTimeout()
+      delayTimeout.current = window.setTimeout(() => {
+        setInnerSpinning(true);
+      }, delay)
+    } else {
+      setInnerSpinning(spinning)
+    }
+
+  }, [spinning, delay])
+
+  const clearDelayTimeout = () => {
+    if (delayTimeout.current) {
+      clearTimeout(delayTimeout.current);
+    }
+  }
+
+  function isNestedPattern() {
+    return !!(props && props.children);
+  }
+
+  // Render
+  const spinClassName = classNames(
+    prefixCls,
+    {
+      [`${prefixCls}-sm`]: size === 'small',
+      [`${prefixCls}-lg`]: size === 'large',
+      [`${prefixCls}-spinning`]: innerSpinning,
+      [`${prefixCls}-show-text`]: !!tip
+    },
+    className
+  );
+
+  // fix https://fb.me/react-unknown-prop
+  const divProps = restProps;
+
+  const spinElement = (
+    <div {...divProps} className={spinClassName}>
+      {renderIndicator(props)}
+      {tip ? <span className={`${prefixCls}-text`}>{tip}</span> : null}
+    </div>
+  );
+
+  if (isNestedPattern()) {
+    let animateClassName = prefixCls + '-nested-loading';
+    if (wrapperClassName) {
+      animateClassName += ' ' + wrapperClassName;
+    }
+    const nestedClassName = classNames({
+      [`${prefixCls}-nested`]: true,
+      [`${prefixCls}-blur`]: innerSpinning
+    });
+    return (
+      <Animate
+        {...divProps}
+        component="div"
+        className={animateClassName}
+        style={null}
+        transitionName="fade"
+      >
+        {innerSpinning && <div key="loading">{spinElement}</div>}
+        <div className={nestedClassName} key="nested">
+          {props.children}
+        </div>
+      </Animate>
+    );
+  }
+
+  return spinElement;
+}
+
+Spin.defaultProps = {
+  prefixCls: 'fishd-spin',
+  spinning: true,
+  size: 'default' as SpinSize,
+  wrapperClassName: ''
+}
+
+Spin.Container = Container;
+Spin.TextLoading = TextLoading;
+Spin.setDefaultIndicator = (indicator) => {
+  defaultIndicator = indicator;
+}
+
 export default Spin;
--- source/components/Spin/__tests__/index.test.js
+++ source/components/Spin/__tests__/index.test.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { mount, render } from "enzyme";
-import Spin from "../index";
+import React from 'react';
+import { mount, render } from 'enzyme';
+import Spin from '../index';
 
-describe("Spin", () => {
+describe('Spin', () => {
   beforeAll(() => {
     jest.useFakeTimers();
   });
@@ -11,59 +11,58 @@ describe("Spin", () => {
     jest.useRealTimers();
   });
 
-  it("组件的背景样式覆盖", () => {
+  it('组件的背景样式覆盖', () => {
     const wrapper = mount(
-      <Spin style={{ background: "red" }}>
+      <Spin style={{ background: 'red' }}>
         <div>content</div>
-      </Spin>
+      </Spin>,
     );
-    expect(
-      wrapper
-        .find(".fishd-spin-spinning")
-        .at(0)
-        .prop("style").background
-    ).toBe("red");
+    expect(wrapper.find('.fishd-spin-spinning').at(0).prop('style').background).toBe('red');
   });
 
-  it("文本指示器", () => {
-    const loadingText = "cumstom loading text";
-    const wrapper = render(
-      <Spin.TextLoading className="myClass" text={loadingText} />
-    );
-    expect(wrapper.hasClass("myClass")).toBeTruthy();
-    expect(wrapper.text()).toBe(loadingText + ".");
+  it('文本指示器', () => {
+    const loadingText = 'cumstom loading text';
+    const wrapper = render(<Spin.TextLoading className="myClass" text={loadingText} />);
+    expect(wrapper.hasClass('myClass')).toBeTruthy();
+    expect(wrapper.text()).toBe(loadingText + '.');
   });
 
-  it("自定义 indicator", () => {
+  it('自定义 indicator', () => {
     const customIndicator = <div className="custom-indicator" />;
     const wrapper = render(<Spin indicator={customIndicator} />);
     expect(wrapper).toMatchSnapshot();
   });
 
-  it("受 spinning 参数控制", () => {
+  it('受 spinning 参数控制', () => {
     const wrapper = mount(<Spin spinning={false} />);
-    expect(wrapper.instance().state.spinning).toBe(false);
+    expect(wrapper.find('.fishd-spin').hasClass('fishd-spin-spinning')).toBe(false);
+
     wrapper.setProps({ spinning: true });
-    expect(wrapper.instance().state.spinning).toBe(true);
+    wrapper.update();
+    expect(wrapper.find('.fishd-spin').hasClass('fishd-spin-spinning')).toBe(true);
   });
 
-  it("受 spinning 参数和 delay 参数控制", () => {
+  it('受 spinning 参数和 delay 参数控制', () => {
     const wrapper = mount(<Spin spinning={false} delay={300} />);
-    expect(wrapper.instance().state.spinning).toBe(false);
+    expect(wrapper.find('.fishd-spin').hasClass('fishd-spin-spinning')).toBe(false);
 
     wrapper.setProps({ spinning: true });
     jest.advanceTimersByTime(280);
-    expect(wrapper.instance().state.spinning).toBe(false);
+    expect(wrapper.find('.fishd-spin').hasClass('fishd-spin-spinning')).toBe(false);
 
     jest.advanceTimersByTime(20);
-    expect(wrapper.instance().state.spinning).toBe(true);
+    wrapper.update();
+    expect(wrapper.find('.fishd-spin').hasClass('fishd-spin-spinning')).toBe(true);
+
     jest.advanceTimersByTime(20);
-    expect(wrapper.instance().state.spinning).toBe(true);
+    wrapper.update();
+    expect(wrapper.find('.fishd-spin').hasClass('fishd-spin-spinning')).toBe(true);
 
     wrapper.setProps({ spinning: false });
-    expect(wrapper.instance().state.spinning).toBe(false);
+    wrapper.update();
+    expect(wrapper.find('.fishd-spin').hasClass('fishd-spin-spinning')).toBe(false);
 
     jest.advanceTimersByTime(20);
-    expect(wrapper.instance().state.spinning).toBe(false);
+    expect(wrapper.find('.fishd-spin').hasClass('fishd-spin-spinning')).toBe(false);
   });
 });
