e60370bbd48c871d3f4034394b546383504c0679
zhangyu <zhangyu59@corp.netease.com>
2022-04-06 16:26:12 +0800

    feat: 图标整理

--- source/components/RichEditor/src/quillCore/themes/snow.js
+++ source/components/RichEditor/src/quillCore/themes/snow.js
@@ -90,7 +90,7 @@ class SnowTheme extends BaseTheme {
 
   extendToolbar(toolbar) {
     toolbar.container.classList.add('ql-snow');
-    // this.buildButtons(toolbar.container.querySelectorAll('button'), icons);
+    this.buildButtons(toolbar.container.querySelectorAll('button'), icons);
     // this.buildPickers(toolbar.container.querySelectorAll('select'), icons);
     this.tooltip = new SnowTooltip(this.quill, this.options.bounds);
     if (toolbar.container.querySelector('.ql-link')) {
