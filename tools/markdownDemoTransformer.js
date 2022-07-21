44779ccb44ef411d3fc6a5f7c39a5c63e6e6ebc2
hangaoke <188869009@qq.com>
2022-07-21 11:34:39 +0800

    build: 编译工具升级

--- tools/markdownDemoTransformer.js
+++ tools/markdownDemoTransformer.js
@@ -235,7 +235,7 @@ function transformCode(codes, filename) {
     import PropTypes from 'prop-types';
     // Table 组件的“可伸缩列” Demo 使用
     ${components.includes('Table') ? `import { Resizable } from 'react-resizable'` : ''};
-    ${components.length ? `import {${components.join(',')}} from '../../../source/components/index.js';` : ''}
+    ${components.length ? `import {${components.join(',')}} from '../../../source/components/index.tsx';` : ''}
 
     ${classNames.map((classname, index) => `let ${classname} = (() => {
       ${codeBodys[index]}
